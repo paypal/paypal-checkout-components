31fc60112c234370fbb206583b651732718e7977
Daniel Brain <dbrain@paypal.com>
2016-06-06 17:24:27 -0700

    Make source clearer

--- publish.sh
+++ publish.sh
@@ -7,8 +7,8 @@ gulp build;
 git add dist;
 git commit -m "Dist" || echo "Nothing to distribute";
 
-mversion patch -m '%s';
+# mversion patch -m '%s';
 
 git push;
-git push --tags;
-npm publish;
+# git push --tags;
+# npm publish;
