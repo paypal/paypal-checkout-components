d1b9036212f6c9f416f3123292c32c982621871e
Daniel Brain <dbrain@paypal.com>
2016-07-22 19:10:01 -0700

    Do not show content behind iframe

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ cp -r ~/nfra/post-robot/src/* node_modules/post-robot/src/ &&
 cp -r ~/nfra/xcomponent/src/* node_modules/xcomponent/src/ &&
 cp -r ~/nfra/beaver-logger/client/* node_modules/beaver-logger/client/ &&
 gulp webpack && 
-scp -r {dist,demo} xotools:public_html/icv4
+scp -r {dist,demo,live_demo} xotools:public_html/icv4
+scp -r {dist,demo,live_demo} root@bluesuncorp.co.uk:/var/www/html/icv4
