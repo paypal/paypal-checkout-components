92414402a3cb022c7071bd225b93eb211104d011
Daniel Brain <dbrain@paypal.com>
2016-08-17 10:39:04 -0700

    Dist

--- dist/paypal.checkout.v4.0.5.js
+++ dist/paypal.checkout.v4.0.5.js
@@ -137,7 +137,7 @@
             tag: "paypal-button",
             name: "ppbutton",
             get version() {
-                return _config.config.ppobjects ? "4" : "4.0.5";
+                return _config.config.ppobjects ? "4.0.5" : "4.0.5";
             },
             defaultEnv: "production",
             envUrls: {
@@ -6082,7 +6082,7 @@
             value: true
         });
         var config = exports.config = {
-            scriptUrl: "//www.paypalobjects.com/api/" + "paypal.checkout.v4.js",
+            scriptUrl: "//www.paypalobjects.com/api/" + "paypal.checkout.v4.0.5.js",
             ppobjects: false,
             env: "production",
             state: "ppxo_xcomponent",
@@ -6160,7 +6160,7 @@
             tag: "paypal-checkout",
             name: "ppcheckout",
             get version() {
-                return _config.config.ppobjects ? "4" : "4.0.5";
+                return _config.config.ppobjects ? "4.0.5" : "4.0.5";
             },
             get defaultEnv() {
                 return _config.config.env || "production";
@@ -8070,7 +8070,7 @@
                 });
                 throw new Error("Can not find bridge url for env: " + env);
             }
-            var version = _config.config.ppobjects ? "4" : "4.0.5";
+            var version = _config.config.ppobjects ? "4.0.5" : "4.0.5";
             _src2["default"].openBridge(bridgeUrl + "?xcomponent=1&version=" + version + "&env=" + _config.config.env);
         }
     } ]);
