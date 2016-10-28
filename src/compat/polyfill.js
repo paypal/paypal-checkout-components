5ad429b073360e2b33c90ea09a024cb7ad1056a6
Daniel Brain <dbrain@paypal.com>
2016-10-27 18:54:30 -0700

    Add iterator shim

--- src/compat/polyfill.js
+++ src/compat/polyfill.js
@@ -1,10 +1,19 @@
 
-import Symbol from 'es6-symbol';
+import Symbol from 'es6-symbol/polyfill';
+import ArrayIterator from 'es6-iterator/array';
 import { SyncPromise as Promise } from 'sync-browser-mocks/src/promise';
 
 if (!window.Symbol) {
     window.Symbol = Symbol;
 }
+
+if (!Array.prototype[Symbol.iterator]) {
+    Array.prototype[Symbol.iterator] = function iterator() { // eslint-disable-line
+        return new ArrayIterator(this);
+    };
+}
+
+window.Symbol = Symbol;
 
 if (!window.Promise) {
     window.Promise = Promise;
