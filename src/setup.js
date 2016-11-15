a77c10a7d42894964e8c9bd2c81bf174762dc86e
Daniel Brain <dbrain@paypal.com>
2016-11-14 18:13:41 -0800

    Fix gitignore rule

--- src/setup.js
+++ src/setup.js
@@ -84,7 +84,7 @@ function getCurrentScript() {
     let scripts = Array.prototype.slice.call(document.getElementsByTagName('script'));
 
     for (let script of scripts) {
-        if (script.src && script.src.replace(/^https?:/, '') === config.scriptUrl || script.hasAttribute('data-paypal-checkout')) {
+        if (script.src && script.src.replace(/^https?:/, '').split('?')[0] === config.scriptUrl || script.hasAttribute('data-paypal-checkout')) {
             return script;
         }
     }
