8b56a9f98551dc92cbc186367799f7a9f9fa4ad2
Daniel Brain <dbrain@paypal.com>
2017-01-27 14:00:31 -0800

    Use vanilla js for mark example

--- sampleApp/public/js/mark.js
+++ sampleApp/public/js/mark.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     jwtCsrf.patchXhr();
 
     var fn = markIntegration.toString();
-    fn = fn.replace('function markIntegration() {', '');
+    fn = fn.replace('function markIntegration() {\n', '');
     fn = fn.slice(0, -1);
 
     editor.setValue(fn, -1);
@@ -25,67 +25,61 @@ $(document).ready(function () {
 });
 
 function markIntegration() {
-var buttonContainer = $('#myButtonContainer');
 
-// only for the first time render
-renderPaypalButton();
+var radioFields           = document.body.querySelectorAll('input[name=paymentOption]');
+var paypalButtonContainer = document.body.querySelector('#paypalButton');
+var cardButtonContainer   = document.body.querySelector('#cardButton');
 
-// watching the radio button input
-$('input:radio[name=paymentOption]').change(function onChange() {
-    buttonContainer.empty();
-
-    var selection = $('input[name=paymentOption]:checked').val();
-
-    if (selection === 'Sole') {
-        renderVisaButton();
-    } else if (selection === 'Mark') {
-        renderPaypalButton();
-    }
-});
-
-function renderVisaButton() {
-    var visaButton = $('<button class="btn-primary">Checkout with Generic!</button>').click(function () {
-
-        // dummy start
-        alert('LAUNCHING ALTERNATE CHECKOUT FLOW');
-    });
-
-    buttonContainer.append(visaButton);
+function showPayPalButton() {
+    cardButtonContainer.style.display   = 'none';
+    paypalButtonContainer.style.display = 'block';
 }
 
+function showCardButton() {
+    paypalButtonContainer.style.display = 'none';
+    cardButtonContainer.style.display   = 'block';
+}
 
-function renderPaypalButton() {
+paypal.Button.render({
 
-    paypal.Button.render({
-        env: 'sandbox',
-        client: {
-            sandbox: 'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R'
-        },
-        payment: function() {
-            return paypal.rest.payment.create(this.props.env, this.props.client, {
-                transactions: [
-                    {
-                        amount: {
-                            total:    '1.00',
-                            currency: 'USD'
-                        }
-                    }
-                ]
-            });
-        },
+    env: 'sandbox',
 
-        onAuthorize: function(data, actions) {
+    client: {
+        sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+        production: 'Aco85QiB9jk8Q3GdsidqKVCXuPAAVbnqm0agscHCL2-K2Lu2L6MxDU2AwTZa-ALMn_N0z-s2MXKJBxqJ'
+    },
 
-            return actions.payment.execute().then(function() {
+    payment: function() {
+        return paypal.rest.payment.create(this.props.env, this.props.client, {
+            transactions: [
+                {
+                    amount: { total:    '1.00', currency: 'USD' }
+                }
+            ]
+        });
+    },
 
-                console.log('The payment was completed!');
-            });
-        },
-        onCancel: function(data) {
-            console.log('The payment was cancelled!');
+    onAuthorize: function(data, actions) {
+        return actions.payment.execute().then(function() {
+            console.log('The payment was completed!');
+        });
+    }
+
+}, '#paypalButton');
+
+showPayPalButton();
+
+radioFields.forEach(function(el) {
+    el.addEventListener('change', function(event) {
+
+        if (event.target.value === 'paypal') {
+            showPayPalButton();
         }
 
-    }, '#myButtonContainer');
+        if (event.target.value === 'card') {
+            showCardButton();
+        }
+    });
+});
 
-}
 }
