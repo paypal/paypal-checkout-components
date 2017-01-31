e88a11e4ef5a47d17af7b97d2469526ac891b544
Daniel Brain <daniel@bluesuncorp.co.uk>
2017-01-30 23:08:55 -0800

    Remove redundant window.console.karma

--- test/test.js
+++ test/test.js
@@ -24,9 +24,3 @@ afterEach(() => {
         // return postRobot.destroyBridges();
     });
 });
-
-window.console.karma = function() {
-    let karma = window.karma || (window.top && window.top.karma) || (window.opener && window.opener.karma);
-    karma.log('debug', arguments);
-    console.log.apply(console, arguments);
-};
