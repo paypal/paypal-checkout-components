36f02cf7c83b065dce8f14a288f282afe3108754
Daniel Brain <dbrain@paypal.com>
2017-02-09 15:34:22 -0800

    Include latest versioned file in dist

--- publish.sh
+++ publish.sh
@@ -5,9 +5,11 @@ set -e;
 rm -rf node_modules/xcomponent node_modules/post-robot node_modules/sync-browser-mocks node_modules/beaver-logger
 npm install xcomponent post-robot sync-browser-mocks beaver-logger
 
+rm dist/*;
+
 gulp build;
 
-git add dist;
+git add dist --all;
 git commit -m "Dist" || echo "Nothing to distribute";
 
 mversion patch -m '%s';
