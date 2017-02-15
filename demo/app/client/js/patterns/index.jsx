f3b1296a9ac67abc7b97fd0647acc1d891a620db
Daniel Brain <dbrain@paypal.com>
2017-02-14 17:14:30 -0800

    Comment out agreements and braintree demo

--- demo/app/client/js/patterns/index.jsx
+++ demo/app/client/js/patterns/index.jsx
@@ -1,8 +1,8 @@
 
 export * from './client';
 export * from './server';
-export * from './braintree';
-export * from './billing';
+// export * from './braintree';
+// export * from './billing';
 export * from './experience';
 export * from './styles';
 export * from './mark';
