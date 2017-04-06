32f4d2193188c8cbe63655627c98ebe04a1fd453
Daniel Brain <dbrain@paypal.com>
2017-04-06 13:36:23 -0700

    Add cross-domain-safe-weakmap to publish script

--- publish.sh
+++ publish.sh
@@ -2,8 +2,8 @@
 
 set -e;
 
-rm -rf node_modules/xcomponent node_modules/post-robot node_modules/sync-browser-mocks node_modules/beaver-logger
-npm install xcomponent post-robot sync-browser-mocks beaver-logger
+rm -rf node_modules/xcomponent node_modules/post-robot node_modules/sync-browser-mocks node_modules/beaver-logger node_modules/cross-domain-safe-weakmap
+npm install xcomponent post-robot sync-browser-mocks beaver-logger cross-domain-safe-weakmap
 
 npm run postinstall
 
