f0018d2cf350f31601291c60f1107a22085ecccc
Daniel Brain <dbrain@paypal.com>
2017-04-11 16:05:17 -0700

    Add validation demo

--- demo/app/client/js/patterns/index.jsx
+++ demo/app/client/js/patterns/index.jsx
@@ -4,8 +4,9 @@ export * from './server';
 // export * from './braintree';
 // export * from './billing';
 // export * from './experience';
+
 export * from './styles';
 export * from './credit';
 export * from './mark';
 export * from './confirm';
-
+export * from './validation';
