1461248a448336e251566d6d6115b178efcbfc05
Daniel Brain <dbrain@paypal.com>
2017-04-17 18:04:31 -0700

    Simplify child loader

--- src/loader/interface.js
+++ src/loader/interface.js
@@ -5,5 +5,3 @@ import { integrationResponder } from './load';
 export function onLoadCheckoutIntegration(callback : (err : ?Error, result : ?mixed) => void) : void {
     return integrationResponder.listen(callback);
 }
-
-module.exports.default = module.exports;
