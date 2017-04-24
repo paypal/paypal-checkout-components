30de0eb4e1bd1861a777e9d8e80d612680fac712
Daniel Brain <dbrain@paypal.com>
2017-04-24 16:38:43 -0700

    Add tests for error cases in payment method

--- test/tests/button/error.js
+++ test/tests/button/error.js
@@ -19,6 +19,84 @@ for (let flow of [ 'popup', 'iframe' ]) {
             window.paypal.Checkout.contexts.iframe = false;
         });
 
+        it.only('should render button, render checkout, and throw an error in payment', (done) => {
+
+            return window.paypal.Button.render({
+
+                test: { flow, action: 'error' },
+
+                payment() : string | SyncPromise<string> {
+                    throw new Error('error');
+                },
+
+                onError(err) : void {
+                    assert.isOk(err instanceof Error);
+                    return done();
+                },
+
+                onAuthorize() : void {
+                    return done(new Error('Expected onCancel to not be called'));
+                },
+
+                onCancel() : void {
+                    return done(new Error('Expected onCancel to not be called'));
+                }
+
+            }, '#testContainer');
+        });
+
+        it('should render button, render checkout, and return a rejected promise in payment', (done) => {
+
+            return window.paypal.Button.render({
+
+                test: { flow, action: 'error' },
+
+                payment() : string | SyncPromise<string> {
+                    return window.paypal.Promise.reject(new Error('error'));
+                },
+
+                onError(err) : void {
+                    assert.isOk(err instanceof Error);
+                    return done();
+                },
+
+                onAuthorize() : void {
+                    return done(new Error('Expected onCancel to not be called'));
+                },
+
+                onCancel() : void {
+                    return done(new Error('Expected onCancel to not be called'));
+                }
+
+            }, '#testContainer');
+        });
+
+        it('should render button, render checkout, and call reject in payment', (done) => {
+
+            return window.paypal.Button.render({
+
+                test: { flow, action: 'error' },
+
+                payment(resolve, reject) {
+                    reject(new Error('error'));
+                },
+
+                onError(err) : void {
+                    assert.isOk(err instanceof Error);
+                    return done();
+                },
+
+                onAuthorize() : void {
+                    return done(new Error('Expected onCancel to not be called'));
+                },
+
+                onCancel() : void {
+                    return done(new Error('Expected onCancel to not be called'));
+                }
+
+            }, '#testContainer');
+        });
+
         it('should render button, then fall back and complete the payment', (done) => {
 
             return window.paypal.Button.render({
