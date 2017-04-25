673a2fc69d457e86a4b37af5aed6bf865c36b4c3
Daniel Brain <dbrain@paypal.com>
2017-04-24 17:42:13 -0700

    Fix console.karma

--- test/tests/common.js
+++ test/tests/common.js
@@ -206,10 +206,11 @@ $mockEndpoint.register({
 }).listen();
 
 
+window.karma = window.karma || (window.top && window.top.karma) || (window.parent && window.parent.karma) || (window.opener && window.opener.karma);
+
 window.console.karma = function() {
-    let karma = window.karma || (window.top && window.top.karma) || (window.parent && window.parent.karma) || (window.opener && window.opener.karma);
-    if (karma) {
-        karma.log('debug', arguments);
+    if (window.karma) {
+        window.karma.log('debug', arguments);
     }
     console.log.apply(console, arguments);
 };
