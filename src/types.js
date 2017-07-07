d444a521c137caa429640d7522d6f628a4196cfd
Daniel Brain <dbrain@paypal.com>
2017-07-07 11:14:45 -0700

    Add flow declaration for __IE_POPUP_SUPPORT__

--- src/types.js
+++ src/types.js
@@ -5,3 +5,4 @@ declare var __FILE_NAME__ : string;
 declare var __TEST__ : string;
 declare var __DEFAULT_LOG_LEVEL__ : string;
 declare var __LEGACY_SUPPORT__ : boolean;
+declare var __IE_POPUP_SUPPORT__ : boolean;
