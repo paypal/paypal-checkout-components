d2a32cad9dd5951da38322a177a61189530354e3
Daniel Brain <dbrain@paypal.com>
2017-07-19 20:46:07 -0700

    Log pptm load error

--- src/lib/pptm.js
+++ src/lib/pptm.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import { config, FPTI, PPTM_ID } from '../config';
-import { loadScript, getElement } from '../lib/dom';
+import { loadScript, getElement, stringifyError } from '../lib';
 import * as $logger from 'beaver-logger/client';
 
 export function createPptmScript() {
@@ -27,9 +27,13 @@ export function createPptmScript() {
     const fullUrl = `${config.pptmUrl}?id=${window.location.hostname}&t=xo`;
 
     loadScript(fullUrl, 0, { async: true, id: PPTM_ID }).then(() => {
+
         $logger.track({
             [ FPTI.KEY.STATE ]: FPTI.STATE.PPTM,
             [ FPTI.KEY.TRANSITION ]: FPTI.TRANSITION.PPTM_LOADED
         });
+
+    }).catch(err => {
+        $logger.error('pptm_script_error', { error: stringifyError(err) });
     });
 }
