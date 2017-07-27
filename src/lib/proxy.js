5ccb8e09c8d88ea44a5a574778afd5879ff5d1e6
Daniel Brain <dbrain@paypal.com>
2017-07-27 18:55:21 -0400

    Send proxy regardless of current domain

--- src/lib/proxy.js
+++ src/lib/proxy.js
@@ -1,37 +1,35 @@
 
 import { on, send } from 'post-robot/src';
-import { isWindowClosed } from 'cross-domain-utils/src';
+import { isWindowClosed, getDomain } from 'cross-domain-utils/src';
 
 import { config } from '../config';
-import { noop, isPayPalDomain } from './util';
+import { noop } from './util';
 
 export function proxyMethod(name : string, win : any, originalMethod : Function) : Function {
 
-    if (isPayPalDomain()) {
+    if (getDomain() === config.paypalDomain) {
 
         if (win) {
             send(win, `proxy_${name}`, { originalMethod }).catch(noop);
         }
 
         return originalMethod;
-
-    } else {
-
-        let methods = [];
-
-        on(`proxy_${name}`, { domain: config.paypal_domain_regex }, ({ data }) => {
-            methods.push(data.originalMethod);
-        });
-
-        return function() : mixed {
-
-            methods = methods.filter(method => !isWindowClosed(method.source));
-
-            if (methods.length) {
-                return methods[0].apply(this, arguments);
-            }
-
-            return originalMethod.apply(this, arguments);
-        };
     }
+
+    let methods = [];
+
+    on(`proxy_${name}`, { domain: config.paypal_domain_regex }, ({ data }) => {
+        methods.push(data.originalMethod);
+    });
+
+    return function() : mixed {
+
+        methods = methods.filter(method => !isWindowClosed(method.source));
+
+        if (methods.length) {
+            return methods[0].apply(this, arguments);
+        }
+
+        return originalMethod.apply(this, arguments);
+    };
 }
