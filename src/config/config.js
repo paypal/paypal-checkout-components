cd33efc481a28449e62af1c718be172d22b7be89
Daniel Brain <dbrain@paypal.com>
2017-07-27 18:57:18 -0400

    Enable per-domain settings

--- src/config/config.js
+++ src/config/config.js
@@ -40,7 +40,20 @@ export let config = {
     domain_settings: {
         kjalrclisa: {
             log_domain_prefix: true,
-            custom_button_selector: '.paypal-payment-option'
+            custom_button_selector: '.paypal-payment-option',
+            ie_full_page: true,
+            pre_click_full_page: true,
+            memoize_payment: true
+        },
+        vmlxojsbya: {
+            ie_full_page: true,
+            pre_click_full_page: true,
+            memoize_payment: true
+        },
+        ffrlgagqaa: {
+            ie_full_page: true,
+            pre_click_full_page: true,
+            memoize_payment: true
         }
     },
 
