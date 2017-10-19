b97b57c60eac98ed981a1ede3bc68477c65b9723
Daniel Brain <dbrain@paypal.com>
2017-10-18 17:29:35 -0700

    Do not show paypal in horizontal dual credit button

--- src/components/button/templates/component/style/labels.js
+++ src/components/button/templates/component/style/labels.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import { BUTTON_STYLE } from '../../style';
-import { BUTTON_LABEL, BUTTON_LOGO, BUTTON_SIZE } from '../../../constants';
+import { BUTTON_LABEL, BUTTON_LOGO, BUTTON_SIZE, BUTTON_LAYOUT, BUTTON_NUMBER } from '../../../constants';
 
 import { CLASS } from './class';
 
@@ -9,6 +9,10 @@ export let labelStyle = `
 
     .${ CLASS.BUTTON } .${ CLASS.LABEL }-${ BUTTON_LABEL.CREDIT } .${ CLASS.TEXT } {
         display: none !important;
+    }
+
+    .${ CLASS.BUTTON }.${ CLASS.LAYOUT }-${ BUTTON_LAYOUT.HORIZONTAL }.${ CLASS.NUMBER }-${ BUTTON_NUMBER.MULTIPLE }.${ CLASS.LABEL }-${ BUTTON_LABEL.CREDIT } .${ CLASS.LOGO }.${ CLASS.LOGO }-${ BUTTON_LOGO.PAYPAL } {
+        display: none;
     }
 
     @media only screen and (max-width : ${ BUTTON_STYLE[BUTTON_SIZE.SMALL].minWidth }px) {
