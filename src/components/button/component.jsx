3cc440de77f214c06e87006876db4ddad1473bf8
Daniel Brain <dbrain@paypal.com>
2017-12-11 18:24:40 -0800

    Add demo_sandbox_client_id and demo_production_client_id

--- src/components/button/component.jsx
+++ src/components/button/component.jsx
@@ -180,6 +180,18 @@ export let Button : Component<ButtonOptions> = create({
                 } else if (!ZalgoPromise.isPromise(client[env])) {
                     throw new Error(`Expected client token to be either a string or a promise`);
                 }
+            },
+
+            decorate(client : Object) : Object {
+                if (client && client.sandbox === 'demo_sandbox_client_id') {
+                    client.sandbox = 'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R';
+                }
+
+                if (client && client.production === 'demo_production_client_id') {
+                    client.production = 'Aco85QiB9jk8Q3GdsidqKVCXuPAAVbnqm0agscHCL2-K2Lu2L6MxDU2AwTZa-ALMn_N0z-s2MXKJBxqJ';
+                }
+
+                return client;
             }
         },
 
