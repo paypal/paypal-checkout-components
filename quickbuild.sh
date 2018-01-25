8f8dca026c72b89d35a4126a816b72febac3f0d8
Daniel Brain <dbrain@paypal.com>
2018-01-25 11:02:57 -0800

    Add webpack-base task to build just checkout.js

--- quickbuild.sh
+++ quickbuild.sh
@@ -6,6 +6,6 @@ cp -r ~/nfra/xcomponent/src/* node_modules/xcomponent/src/ &&
 cp -r ~/nfra/beaver-logger/client/* node_modules/beaver-logger/client/ &&
 cp -r ~/nfra/cross-domain-utils/src/* node_modules/cross-domain-utils/src/ &&
 cp -r ~/nfra/zalgo-promise/src/* node_modules/zalgo-promise/src/ &&
-npm run build &&
+npm run webpack-base &&
 rsync -avz dist/checkout.js root@bluesuncorp.co.uk:/var/www/html/icv4/dist/checkout.js &&
 git checkout dist
