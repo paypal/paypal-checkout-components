4f290a6013160f532a67ad139b57bd0c9596fa13
Daniel Brain <dbrain@paypal.com>
2018-02-07 15:08:13 +0800

    Switch interface back to non-lazy mode

--- src/interface.js
+++ src/interface.js
@@ -5,6 +5,7 @@ import { ZalgoPromise } from 'zalgo-promise/src';
 import * as _postRobot from 'post-robot/src'; // eslint-disable-line import/no-namespace
 
 import { isPayPalDomain } from './lib';
+import { Checkout as _Checkout } from './checkout';
 
 export const postRobot = _postRobot;
 export { ZalgoPromise as Promise } from 'zalgo-promise/src';
@@ -39,55 +40,14 @@ export let PayPalCheckout;
 export let destroyAll;
 export let enableCheckoutIframe;
 
+function _enableCheckoutIframe() {
+    _Checkout.contexts.iframe = true;
+}
+
 if (isPayPalDomain() || __TEST__) {
 
-    let CheckoutPromise = import('./checkout').then(mod => mod.Checkout);
-
-    Checkout = {
-
-        render: (...args) =>
-            CheckoutPromise.then(Result =>
-                Result.render(...args)),
-
-        renderTo: (...args) =>
-            CheckoutPromise.then(Result =>
-                Result.renderTo(...args)),
-
-        renderPopupTo: (...args) =>
-            CheckoutPromise.then(Result =>
-                Result.renderPopupTo(...args)),
-
-        renderIframeTo: (...args) =>
-            CheckoutPromise.then(Result =>
-                Result.renderIframeTo(...args)),
-
-        contexts: {
-            set iframe(enabled) {
-                CheckoutPromise.then(Result => {
-                    Result.contexts.iframe = enabled;
-                });
-            },
-            set popup(enabled) {
-                CheckoutPromise.then(Result => {
-                    Result.contexts.popup = enabled;
-                });
-            }
-        },
-
-        props: {
-            set timeout(timeout) {
-                CheckoutPromise.then(Result => {
-                    Result.props.timeout = timeout;
-                });
-            }
-        }
-    };
-
-    enableCheckoutIframe = () => {
-        CheckoutPromise.then(Result => {
-            Result.contexts.iframe = true;
-        });
-    };
-
+    Checkout = _Checkout;
+    PayPalCheckout = _Checkout;
+    enableCheckoutIframe = _enableCheckoutIframe;
     destroyAll = _destroyAll;
 }
