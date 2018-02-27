99c3322c82ad73668f4dbc4b394097723be5561e
Daniel Brain <dbrain@paypal.com>
2018-02-27 15:27:58 -0800

    Better error messaging for multiple load

--- src/load.js
+++ src/load.js
@@ -5,15 +5,20 @@ import { extendNamespace } from './lib/namespace';
 
 if (window.paypal && window.paypal.version === __MINOR_VERSION__) {
 
-    let error = 'PayPal Checkout Integration Script already loaded on page';
+    beacon('bootstrap_already_loaded_same_version', {
+        version: __MINOR_VERSION__
+    });
 
-    if (window.console) {
-        if (window.console.warn) {
-            window.console.warn(error);
-        } else {
-            window.console.log(error);
-        }
-    }
+    throw new Error(`PayPal Checkout Integration Script with same version (${ __MINOR_VERSION__ }) already loaded on page`);
+
+} else if (window.paypal && window.paypal.version && window.paypal.version !== __MINOR_VERSION__ && window.paypal.Button && window.paypal.Button.render) {
+
+    beacon('bootstrap_already_loaded_different_version', {
+        existingVersion: window.paypal.version,
+        version:         __MINOR_VERSION__
+    });
+
+    throw new Error(`PayPal Checkout Integration Script with different version (${ window.paypal.version }) already loaded on page, current version: ${ __MINOR_VERSION__ }`);
 
 } else {
 
