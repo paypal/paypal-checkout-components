7e7fc0b9edb7730cd1fcd999a7cdd7f66efe902c
Daniel Brain <chuntheunavoidable@gmail.com>
2018-02-28 22:05:13 -0800

    Increase screenshot jasmine timeout

--- test/screenshot/screenshot.test.js
+++ test/screenshot/screenshot.test.js
@@ -22,7 +22,7 @@ const USER_AGENTS = {
     iphone6: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
 };
 
-jest.setTimeout(50000);
+jest.setTimeout(120000);
 
 let setupBrowserPage = (async () => {
     let { browser, page } = await openPage(await webpackCompile(BASE));
