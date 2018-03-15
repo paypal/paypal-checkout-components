531b69e7e5a78be8a43d3b07ce8dc66bbd838f8f
Daniel Brain <dbrain@paypal.com>
2018-03-15 16:37:27 -0700

    Remove postinstall from publish script

--- publish.sh
+++ publish.sh
@@ -18,8 +18,6 @@ fi;
 rm -rf node_modules/xcomponent node_modules/post-robot node_modules/zalgo-promise node_modules/beaver-logger node_modules/cross-domain-safe-weakmap node_modules/cross-domain-utils
 npm install xcomponent post-robot zalgo-promise beaver-logger cross-domain-safe-weakmap cross-domain-utils
 
-npm run postinstall
-
 rm dist/*;
 
 npm run build;
