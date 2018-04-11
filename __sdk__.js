f91c4f5718b305477fe51ba8443bda2cf7d8ad43
Daniel Brain <dbrain@paypal.com>
2018-04-10 17:26:27 -0700

    Add __sdk__.js

---
+++ __sdk__.js
@@ -0,0 +1,20 @@
+/* @flow */
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+
+    modules: {
+        'buttons': './src/index'
+    },
+
+    staticNamespace: 'PAYPAL_CHECKOUT',
+
+    configQuery: `
+
+    `,
+
+    // Configure features specific to this module
+    features: {
+
+    }
+};
