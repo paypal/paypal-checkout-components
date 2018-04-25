a8b8a0676cda953e10fc433ff10aa2c3a52b8c87
Daniel Brain <dbrain@paypal.com>
2018-04-24 18:08:16 -0700

    Upgrade common client

--- __sdk__.js
+++ __sdk__.js
@@ -2,8 +2,9 @@
 /* eslint unicorn/filename-case: 0, import/unambiguous: 0, import/no-commonjs: 0 */
 
 module.exports = {
-
+    
     'buttons': {
-        entry: './src/index'
-    }
+        entry: './src/index',
+        staticNamespace: '__paypal_checkout__'
+    }    
 };
