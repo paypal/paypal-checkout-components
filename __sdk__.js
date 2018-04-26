a76a57ce5a6c923e4005f5848c06fd02c8f351b1
Daniel Brain <dbrain@paypal.com>
2018-04-26 16:22:09 -0700

    Revert "Upgrade common client"
    This reverts commit c73a473788ba0d0895f319153a7d721adadbd386.

--- __sdk__.js
+++ __sdk__.js
@@ -2,9 +2,8 @@
 /* eslint unicorn/filename-case: 0, import/unambiguous: 0, import/no-commonjs: 0 */
 
 module.exports = {
-    
+
     'buttons': {
-        entry: './src/index',
-        staticNamespace: '__paypal_checkout__'
-    }    
+        entry: './src/index'
+    }
 };
