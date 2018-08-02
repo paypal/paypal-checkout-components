8cac8decf2720f9f7820861aa0a7662292d59afa
Jon McIntosh <jmcintosh@paypal.com>
2018-08-02 11:26:55 -0700

    test(elv): Horizontal, not vertical!

--- test/tests/button/funding.js
+++ test/tests/button/funding.js
@@ -574,14 +574,14 @@ describe(`paypal button component funding mix`, () => {
         }, '#testContainer');
     });
 
-    it('should not render elv in vertical layout', (done) => {
+    it('should render elv in vertical layout', (done) => {
 
         window.paypal.Button.render({
 
             test: {
                 onRender({ fundingSources }) {
-                    if (fundingSources.indexOf(window.paypal.FUNDING.ELV) !== -1) {
-                        throw new Error(`Expected elv to not be offered, got ${ JSON.stringify(fundingSources) }`);
+                    if (fundingSources.indexOf(window.paypal.FUNDING.ELV) === -1) {
+                        throw new Error(`Expected elv to be offered, got ${ JSON.stringify(fundingSources) }`);
                     }
 
                     done();
@@ -693,14 +693,14 @@ describe(`paypal button component funding mix`, () => {
         }, '#testContainer');
     });
 
-    it('should render elv in horizontal layout', (done) => {
+    it('should not render elv in horizontal layout', (done) => {
 
         window.paypal.Button.render({
 
             test: {
                 onRender({ fundingSources }) {
-                    if (fundingSources.indexOf(window.paypal.FUNDING.ELV) === -1) {
-                        throw new Error(`Expected elv to be offered, got ${ JSON.stringify(fundingSources) }`);
+                    if (fundingSources.indexOf(window.paypal.FUNDING.ELV) !== -1) {
+                        throw new Error(`Expected elv to not be offered, got ${ JSON.stringify(fundingSources) }`);
                     }
 
                     done();
