40c61c9ee2c6a831ec1fc02bf566a7ffea659f3d
Jon McIntosh <jmcintosh@paypal.com>
2018-08-27 11:21:35 -0700

    fix(missing-uk-cards): Adding Maestro and Discover for en_GB

--- src/resources/cardLogos/index.js
+++ src/resources/cardLogos/index.js
@@ -5,6 +5,7 @@ import { CARD } from '../../constants';
 import visa         from './visa.svg';
 import amex         from './amex.svg';
 import mastercard   from './mastercard.svg';
+import maestro      from './maestro.svg';
 import discover     from './discover.svg';
 import hiper        from './hiper.svg';
 import elo          from './elo.svg';
@@ -14,6 +15,7 @@ export let cardLogos = {
     [ CARD.VISA ]:       visa,
     [ CARD.AMEX ]:       amex,
     [ CARD.MASTERCARD ]: mastercard,
+    [ CARD.MAESTRO ]:    maestro,
     [ CARD.DISCOVER ]:   discover,
     [ CARD.HIPER ]:      hiper,
     [ CARD.ELO ]:        elo,
