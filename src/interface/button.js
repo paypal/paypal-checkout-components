04b2910b1200b33c8fc53c568749f6c89e602f0e
Daniel Brain <dbrain@paypal.com>
2018-08-28 19:25:27 -0700

    Import Checkout component for button to allow renderTo to work

--- src/interface/button.js
+++ src/interface/button.js
@@ -4,12 +4,14 @@ import { ZalgoPromise as Promise } from 'zalgo-promise/src';
 import { attach } from 'paypal-braintree-web-client/src';
 
 import { Button } from '../button';
+import { Checkout } from '../checkout';
 import { FUNDING } from '../constants';
 import { allowIframe } from '../lib';
 
 attach('buttons', () => {
     return {
         Button,
+        Checkout,
         Promise,
         FUNDING,
         allowIframe
