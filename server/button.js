eb484b6d5e2fdba7b7de3251cc2e53b47ae3cd2d
Daniel Brain <dbrain@paypal.com>
2018-09-14 16:50:43 -0700

    Fix logger

--- server/button.js
+++ server/button.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import { undotify, htmlEncode } from 'belter';
-import { logger } from 'beaver-logger-paypal';
+import logger from 'beaver-logger-paypal';
 
 import { getFundingEligibility } from './eligibility';
 import { getSmartButtonClientScript, getSmartButtonRenderScript, startWatchers } from './watcher';
@@ -57,8 +57,8 @@ export function getButtonMiddleware() : (req : ExpressRequest, res : ExpressResp
                 .send(pageHTML);
 
         } catch (err) {
-            logger.error(req, 'smart_button_error', { err: err.stack ? err.stack : err.toString() });
             console.error(err.stack ? err.stack : err); // eslint-disable-line no-console
+            logger.error(req, 'smart_button_error', { err: err.stack ? err.stack : err.toString() });
             res.status(500)
                 .header('content-type', 'text/plain')
                 .send('');
