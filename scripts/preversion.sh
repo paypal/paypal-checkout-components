d36abc5b5c81bca88c301ab76ca512504e7f5f83
Jon McIntosh <jmcintosh@paypal.com>
2018-10-24 15:27:24 -0700

    refactor(publishing): Splitting up publish and version logic

---
+++ scripts/preversion.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e;
+
+# Remove the `dist/` folder; we will re-generate later on
+rm dist/*;
+git checkout dist/;
+
+# Make sure the HEAD is clean
+if ! git diff-files --quiet; then
+    echo "Can not publish with unstaged uncommited changes";
+    exit 1;
+fi;
+
+if ! git diff-index --quiet --cached HEAD; then
+    echo "Can not publish with staged uncommited changes";
+    exit 1;
+fi;
+
+# Re-install just the basics
+rm -rf node_modules/xcomponent node_modules/post-robot node_modules/zalgo-promise node_modules/beaver-logger node_modules/cross-domain-safe-weakmap node_modules/cross-domain-utils node_modules/grumbler-scripts
+npm install xcomponent post-robot zalgo-promise beaver-logger cross-domain-safe-weakmap cross-domain-utils grumbler-scripts
+
+npm run flow;
+npm run lint;
+rm dist/*;
+npm run webpack;
