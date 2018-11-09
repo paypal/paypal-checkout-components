a528d3d08cb919d03e2845412b6bf13e998e1b14
Jon McIntosh <jonmcntsh@gmail.com>
2018-11-09 15:17:31 -0800

    refactor(publishing): fixing postversion script to push both tags and release commit (#941)
    * refactor(publishing): Splitting up publish and version logic
    
    * refactor(publishing): Adding publish message
    
    * refactor(publishing): Adding generated changelog
    
    * refactor(publishing): Clean up
    
    * refactor(publishing): Cleaning up the Changelog a bit
    
    * refactor(publishing): Documentation for releases and contributing
    
    * refactor(publishing): post-version script will push to paypal repo
    
    * refactor(publishing): fixing postversion script to push both tags and release commit

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 
 # Publish and push!
-git push git@github.com:paypal/paypal-checkout.git;
-git push git@github.com:paypal/paypal-checkout.git --tags;
+git push;
+git push --tags;
+git push git@github.com:paypal/paypal-checkout.git master;
 npm publish;
