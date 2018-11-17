aa8c6d8d7c397265b606883b2e53e32da68304fb
Daniel Brain <dbrain@paypal.com>
2018-11-16 16:33:22 -0800

    Import checkout component when requested

--- src/interface/checkout.js
+++ src/interface/checkout.js
@@ -1,1 +1,10 @@
 /* @flow */
+
+import { isPayPalDomain } from 'paypal-braintree-web-client/src';
+
+import { Checkout as _Checkout } from '../checkout';
+
+export let Checkout;
+if (isPayPalDomain()) {
+    Checkout = _Checkout;
+}
