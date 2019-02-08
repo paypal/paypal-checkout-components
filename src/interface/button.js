6eeaa8c5b998fc2853f82c6fcf3a6a546ecc8425
Daniel Brain <daniel@brainblocks.io>
2019-02-07 21:58:37 -0800

    Call getCheckoutComponent to register component in parent window

--- src/interface/button.js
+++ src/interface/button.js
@@ -7,13 +7,15 @@ import { setupLogger, allowIframe as _allowIframe } from '../lib';
 import { getCheckoutComponent } from '../checkout';
 import { getButtonsComponent } from '../buttons';
 
+const CheckoutComponent = getCheckoutComponent();
+
 export const Buttons = getButtonsComponent();
 export let Checkout;
 export let PopupOpenError;
 export let allowIframe;
 
 if (isPayPalDomain()) {
-    Checkout = getCheckoutComponent();
+    Checkout = CheckoutComponent;
     PopupOpenError = _PopupOpenError;
     allowIframe = _allowIframe;
 }
