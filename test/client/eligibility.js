2171be5b8f51ad4100b9aaa4bf7df3f184a49153
Daniel Brain <dbrain@paypal.com>
2019-02-11 12:01:25 -0800

    Add eligibility test

---
+++ test/client/eligibility.js
@@ -0,0 +1,27 @@
+/* @flow */
+/* eslint require-await: off, max-lines: off */
+
+import { wrapPromise } from 'belter/src';
+import { FUNDING } from '@paypal/sdk-constants/src';
+
+import { setupButton } from '../../src';
+
+describe('eligibility cases', () => {
+
+    it('should call xprops.remember with venmo if venmo is eligible', async () => {
+        return await wrapPromise(async ({ expect }) => {
+
+            window.xprops.remember = expect('remember', (sources) => {
+                if (!sources || sources.indexOf(FUNDING.VENMO) === -1) {
+                    throw new Error(`Expected venmo to be remembered`);
+                }
+            });
+
+            await setupButton({
+                [ FUNDING.VENMO ]: {
+                    eligible: true
+                }
+            });
+        });
+    });
+});
