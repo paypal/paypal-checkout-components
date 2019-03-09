e9dc63c883e8121444fa02f4da4874e0b39ce571
Daniel Brain <dbrain@paypal.com>
2019-03-08 17:03:14 -0800

    Allow configuring api base url

--- src/config.js
+++ src/config.js
@@ -1,9 +1,22 @@
 /* @flow */
 
+let BASE_API_URL = '/webapps/hermes/api';
+
+try {
+    if (window.localStorage) {
+        const url = window.localStorage.get('BASE_API_URL');
+        if (url) {
+            BASE_API_URL = url;
+        }
+    }
+} catch (err) {
+    // pass
+}
+
 export const API_URI = {
-    AUTH:     '/webapps/hermes/api/auth',
-    CHECKOUT: '/webapps/hermes/api/checkout',
-    ORDER:    '/webapps/hermes/api/order',
-    PAYMENT:  '/webapps/hermes/api/payment',
+    AUTH:     `${ BASE_API_URL }/auth`,
+    CHECKOUT: `${ BASE_API_URL }/checkout`,
+    ORDER:    `${ BASE_API_URL }/order`,
+    PAYMENT:  `${ BASE_API_URL }/payment`,
     GRAPHQL:  '/graphql'
 };
