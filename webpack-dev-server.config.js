620ed1bfc19d223df67ef7cb34146ba87586e1ce
Daniel Brain <dbrain@paypal.com>
2019-03-15 11:17:25 -0700

    Fix dev server

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -1,31 +1,11 @@
 /* @flow */
 /* eslint unicorn/filename-case: 0 */
-import { LIB } from './webpack.config';
-
-const getNextVersion = () : string => {
-    let version = require('./package.json').version;
-    version = version.split('.');
-    version[2] = (parseInt(version[2], 10) + 1).toString();
-    version = version.join('.');
-    return version;
-};
-
-const nextVer = getNextVersion();
+import { BASE, getWebpackConfig } from './webpack.config';
 
 const webpackConfig = {
-    ...LIB,
-    entry: {
-        [`checkout`]:               './src/index.js',
-        [`checkout.${ nextVer }`]:  './src/index.js'
-    },
-    output: {
-        ...LIB.output,
-        publicPath:   '/',
-        filename:     '[name].js'
-    },
-    devServer: {
-        host: 'localhost.paypal.com'
-    }
+    ...BASE
 };
+
+delete webpackConfig.output.publicPath;
 
 export default webpackConfig;
