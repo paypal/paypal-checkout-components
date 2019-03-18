20d4847fe21edf3ecaf848adb6876d0c3a18f107
Daniel Brain <dbrain@paypal.com>
2019-03-18 09:53:29 -0700

    Fix dev server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -6,17 +6,14 @@ import { getWebpackConfig } from 'grumbler-scripts/config/webpack.config';
 import { testGlobals } from './test/globals';
 import globals from './globals';
 
-const MODULE_NAME = 'paypal';
 const FILE_NAME = 'checkout';
 
 export default getWebpackConfig({
-    entry:         './src/interface/button.js',
+    entry:         './test/paypal.js',
     filename:      `${ FILE_NAME }.js`,
-    modulename:    MODULE_NAME,
     debug:         true,
     minify:        true,
     env:           'sandbox',
-    libraryTarget: 'window',
     vars:          {
         ...globals,
         ...testGlobals,
