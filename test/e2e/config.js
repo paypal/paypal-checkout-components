c6a65f83605d75d8af53f70e57bfdb72cc13010f
Daniel Brain <dbrain@paypal.com>
2019-04-12 13:00:56 -0700

    Add env-vars for configs

--- test/e2e/config.js
+++ test/e2e/config.js
@@ -1,12 +1,13 @@
 /* @flow */
+/* eslint no-process-env: off */
 
 import { argv } from 'yargs';
 
 jest.setTimeout(3 * 60 * 1000);
 
-export const STAGE = argv.stage || 'rpadmanathan19123715404292.qa.paypal.com';
-export const CLIENT_ID = argv['client-id'] || 'ATq4kPMhjTp6qrUQybY0SSz6Es1sO0YDF9f67rt2e-dZx36hHGbV1U9Ek3QRwcHcyyBlHXysRr-uXg18';
-export const DOMAIN = 'https://www.mock-spb-merchant.com';
+export const STAGE = process.env.STAGE || 'msmaster.qa.paypal.com';
+export const CLIENT_ID = process.env.CLIENT_ID || 'ATq4kPMhjTp6qrUQybY0SSz6Es1sO0YDF9f67rt2e-dZx36hHGbV1U9Ek3QRwcHcyyBlHXysRr-uXg18';
+export const DOMAIN = process.env.SPB_DOMAIN || 'https://www.mock-spb-merchant.com';
 
-export const HEADLESS = Boolean(!argv['keep-open']);
-export const DEVTOOLS = HEADLESS && Boolean(argv['dev-tools']);
+export const HEADLESS = process.env.KEEP_OPEN || Boolean(!argv['keep-open']);
+export const DEVTOOLS = HEADLESS && process.env.DEV_TOOLS;
