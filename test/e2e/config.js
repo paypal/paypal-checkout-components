f944365493c83faa26974c3d519d90f94da1b9f3
Daniel Brain <dbrain@paypal.com>
2019-04-12 19:00:04 -0700

    Fix env var bools

--- test/e2e/config.js
+++ test/e2e/config.js
@@ -7,7 +7,7 @@ export const STAGE = process.env.STAGE || 'msmaster.qa.paypal.com';
 export const CLIENT_ID = process.env.CLIENT_ID || 'ATq4kPMhjTp6qrUQybY0SSz6Es1sO0YDF9f67rt2e-dZx36hHGbV1U9Ek3QRwcHcyyBlHXysRr-uXg18';
 export const DOMAIN = process.env.SPB_DOMAIN || 'https://www.mock-spb-merchant.com';
 
-export const HEADLESS = Boolean(process.env.HEADLESS !== '0');
-export const DEVTOOLS = Boolean(HEADLESS && process.env.DEV_TOOLS);
+export const HEADLESS = Boolean(process.env.HEADLESS !== '0' && process.env.HEADLESS !== 'false');
+export const DEVTOOLS = Boolean(HEADLESS && (process.env.DEV_TOOLS === '1' || process.env.DEV_TOOLS === 'true'));
 export const RETRIES = process.env.RETRIES ? parseInt(process.env.RETRIES, 10) : 5;
-export const LOG = Boolean(process.env.LOG === '1');
+export const LOG = Boolean(process.env.LOG === '1' || process.env.LOG === 'true');
