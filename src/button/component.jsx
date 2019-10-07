5867166afd46a8f0830dc9cf4e38a426acfd1082
Daniel Brain <daniel@brainblocks.io>
2019-10-07 10:23:10 -0700

    Disable smart button url throttle

--- src/button/component.jsx
+++ src/button/component.jsx
@@ -147,7 +147,7 @@ export const Button : Component<ButtonOptions> = create({
         const env = props.env || config.env;
         const url = config.buttonUrls[env];
 
-        if (smartThrottle.isEnabled() || props.enableNativeCheckout || env !== ENV.PRODUCTION) {
+        if (props.enableNativeCheckout) {
             return url.replace('/webapps/hermes/button', '/smart/button');
         }
 
