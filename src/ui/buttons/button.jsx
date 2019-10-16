2b229553dae05904c9ca2a904c4a85b2d0f21b62
Serdobolskiy Yehor <egor.serdobolskyi@cashstar.com>
2019-10-16 09:47:39 -0400

    feature/keyboardAccessibility removed traling space

--- src/ui/buttons/button.jsx
+++ src/ui/buttons/button.jsx
@@ -29,7 +29,7 @@ type BasicButtonProps = {|
 export function BasicButton({ fundingSource, style, multiple, locale, env, fundingEligibility, i, nonce, clientAccessToken, onClick = noop } : BasicButtonProps) : ElementNode {
 
     let { color, period, label } = style;
-    
+
     const fundingConfig = getFundingConfig()[fundingSource];
 
     if (!fundingConfig) {
