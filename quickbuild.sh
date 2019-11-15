c04515e327b1bd89a13d4a1c56d53bef6e920ea9
Daniel Brain <dbrain@paypal.com>
2019-11-14 18:33:27 -0800

    Simplify quickbuild

--- quickbuild.sh
+++ quickbuild.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-cp -r ~/nfra/sync-browser-mocks/src/* node_modules/sync-browser-mocks/src/ &&
-cp -r ~/nfra/post-robot/src/* node_modules/post-robot/src/ &&
-cp -r ~/nfra/zoid-v6/src/* node_modules/zoid/src/ &&
-cp -r ~/nfra/beaver-logger/client/* node_modules/beaver-logger/client/ &&
-cp -r ~/nfra/cross-domain-utils/src/* node_modules/cross-domain-utils/src/ &&
-cp -r ~/nfra/zalgo-promise/src/* node_modules/zalgo-promise/src/ &&
 npm run webpack-base &&
 rsync -avz dist/* root@bluesuncorp.co.uk:/var/www/html/icv4/dist/ &&
 git checkout dist
