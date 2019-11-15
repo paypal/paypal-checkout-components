f09ae721dabc96ea3204264a8ec1aaece1e40877
Daniel Brain <dbrain@paypal.com>
2019-11-14 18:33:12 -0800

    Remove loops in button reflow code

--- src/button/template/componentScript.js
+++ src/button/template/componentScript.js
@@ -13,16 +13,6 @@ export function getComponentScript() : () => void {
             HIDDEN:       'hidden'
         };
 
-        function loop(method : Function, delay : number, instances : number) {
-            setTimeout(() => {
-                method();
-                instances -= 1;
-                if (instances) {
-                    loop(method, delay, instances);
-                }
-            }, delay);
-        }
-
         function getElements(selector, parent) : $ReadOnlyArray<HTMLElement> {
             parent = parent || document;
             return Array.prototype.slice.call(parent.querySelectorAll(selector));
@@ -42,36 +32,6 @@ export function getComponentScript() : () => void {
 
         function makeElementInvisible(el : HTMLElement) {
             el.style.visibility = STYLE.HIDDEN;
-        }
-
-        function hasDimensions(el : HTMLElement) : boolean {
-            const rect = el.getBoundingClientRect();
-            return Boolean(rect.height && rect.width);
-        }
-
-        function isHidden(el : HTMLElement) : boolean {
-            const computedStyle = window.getComputedStyle(el);
-            return (!computedStyle || computedStyle.display === STYLE.NONE);
-        }
-
-        function displayedElementsHaveDimensions(elements : $ReadOnlyArray<HTMLElement>) : boolean {
-            return elements.every(el => {
-                return hasDimensions(el) || isHidden(el);
-            });
-        }
-
-        function onDisplay(elements, method) {
-            if (displayedElementsHaveDimensions(elements)) {
-                method();
-                return;
-            }
-
-            const interval = setInterval(() => {
-                if (displayedElementsHaveDimensions(elements)) {
-                    clearInterval(interval);
-                    method();
-                }
-            }, 5);
         }
 
         function isOverflowing(el : HTMLElement) : boolean {
@@ -158,14 +118,8 @@ export function getComponentScript() : () => void {
         toggleOptionals();
         setupTabOutlineEvent();
 
-        onDisplay(images, () => {
-            images.forEach(makeElementVisible);
-            toggleOptionals();
-
-            document.addEventListener('DOMContentLoaded', toggleOptionals);
-            window.addEventListener('load', toggleOptionals);
-            window.addEventListener('resize', toggleOptionals);
-            loop(toggleOptionals, 10, 10);
-        });
+        document.addEventListener('DOMContentLoaded', toggleOptionals);
+        window.addEventListener('load', toggleOptionals);
+        window.addEventListener('resize', toggleOptionals);
     };
 }
