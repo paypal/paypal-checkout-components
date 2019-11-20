95c8c62d3358810db3f3137e14d537f68e09c1a2
Daniel Brain <dbrain@paypal.com>
2019-11-19 17:23:01 -0800

    Log pay_flow cal event

--- src/button/component.jsx
+++ src/button/component.jsx
@@ -840,6 +840,7 @@ export const Button : Component<ButtonOptions> = create({
                         [ FPTI.KEY.BUTTON_SESSION_UID ]: this.props.buttonSessionID
                     });
                     info('button_click');
+                    info(`pay_flow_${ data && data.flow }`);
 
                     track({
                         [ FPTI.KEY.STATE ]:              FPTI.STATE.BUTTON,
