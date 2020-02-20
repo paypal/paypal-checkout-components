cc20240c6402fc67d1ab6d9ba19c3b5ddd21d143
Daniel Brain <dbrain@paypal.com>
2020-02-19 16:16:30 -0800

    Dist

--- dist/smart-payment-buttons.js
+++ dist/smart-payment-buttons.js
@@ -3918,7 +3918,7 @@ window.spb = function(modules) {
                 var _ref2;
                 return (_ref2 = {}).state_name = "smart_button", _ref2.context_type = "button_session_id", 
                 _ref2.context_id = buttonSessionID, _ref2.state_name = "smart_button", _ref2.button_session_id = buttonSessionID, 
-                _ref2.button_version = "2.0.197", _ref2;
+                _ref2.button_version = "2.0.198", _ref2;
             }));
             (function() {
                 if (window.document.documentMode) try {
