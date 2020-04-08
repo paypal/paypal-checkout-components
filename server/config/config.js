ebc8668ca41b94c0e99d09f04e85147873fca7af
Daniel Brain <dbrain@paypal.com>
2020-04-08 12:57:59 -0700

    Update poll interval

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ import { join } from 'path';
 
 import pkg from '../../package.json';
 
-export const MODULE_POLL_INTERVAL = 10 * 60;
+export const MODULE_POLL_INTERVAL = 5 * 60;
 
 export const FNCLS = 'fnparams-dede7cc5-15fd-4c75-a9f4-36c430ee3a99';
 export const FRAUDNET_ID = 'SMART_PAYMENT_BUTTONS';
