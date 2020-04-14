c32845be243c6977c958551353bf0ad997d3525f
Daniel Brain <dbrain@paypal.com>
2020-04-14 11:40:37 -0700

    Add idToken param to checkout

--- src/zoid/checkout/component.jsx
+++ src/zoid/checkout/component.jsx
@@ -98,6 +98,12 @@ export function getCheckoutComponent() : ZoidComponent<CheckoutPropsType> {
                     value:    getCSPNonce
                 },
 
+                idToken: {
+                    type:       'string',
+                    required:   false,
+                    queryParam: 'id-token'
+                },
+
                 buyerCountry: {
                     type:       'string',
                     queryParam: true,
