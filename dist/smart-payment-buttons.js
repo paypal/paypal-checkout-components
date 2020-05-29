71542c5358acd40d342c35eaa84c156d01a332ca
Daniel Brain <daniel@brainblocks.io>
2020-05-28 17:56:45 -0700

    Dist

--- dist/smart-payment-buttons.js
+++ dist/smart-payment-buttons.js
@@ -4538,7 +4538,7 @@ window.spb = function(modules) {
                 var _ref2;
                 return (_ref2 = {}).state_name = "smart_button", _ref2.context_type = "button_session_id", 
                 _ref2.context_id = buttonSessionID, _ref2.state_name = "smart_button", _ref2.button_session_id = buttonSessionID, 
-                _ref2.button_version = "2.0.265", _ref2;
+                _ref2.button_version = "2.0.266", _ref2;
             }));
             (function() {
                 if (window.document.documentMode) try {
