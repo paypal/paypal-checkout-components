b7304cbe5d5ae214f29144f34c04d2d1e7cf1a44
Daniel Brain <dbrain@paypal.com>
2020-08-26 12:56:03 -0700

    Add new validation logs

--- src/button/orders.js
+++ src/button/orders.js
@@ -80,6 +80,8 @@ function isValidMerchantIDs(merchantIDs : $ReadOnlyArray<string>, payees : $Read
 }
 
 export function validateOrder(orderID : string, { env, clientID, merchantID, expectedCurrency, expectedIntent } : ValidateOptions) : ZalgoPromise<void> {
+    const logger = getLogger();
+    
     // eslint-disable-next-line complexity
     return getSupplementalOrderInfo(orderID).then(order => {
         const cart = order.checkoutSession.cart;
@@ -87,25 +89,28 @@ export function validateOrder(orderID : string, { env, clientID, merchantID, exp
         const currency = cart.amounts && cart.amounts.total.currencyCode;
 
         if (intent !== expectedIntent) {
+            logger.warn('smart_button_validation_error_incorrect_intent', { intent, expectedIntent });
             throw new Error(`Expected intent from order api call to be ${ expectedIntent }, got ${ intent }. Please ensure you are passing ${ SDK_QUERY_KEYS.INTENT }=${ intent } to the sdk url. https://developer.paypal.com/docs/checkout/reference/customize-sdk/`);
         }
 
         if (currency && currency !== expectedCurrency) {
+            logger.warn('smart_button_validation_error_incorrect_currency', { currency, expectedCurrency });
             throw new Error(`Expected currency from order api call to be ${ expectedCurrency }, got ${ currency }. Please ensure you are passing ${ SDK_QUERY_KEYS.CURRENCY }=${ currency } to the sdk url. https://developer.paypal.com/docs/checkout/reference/customize-sdk/`);
         }
 
         if (!merchantID || merchantID.length === 0) {
+            logger.warn('smart_button_validation_error_no_merchant_id');
             throw new Error(`Could not determine correct merchant id`);
         }
 
         const payees = order.checkoutSession.payees;
 
         if (!payees) {
-            return getLogger().warn(`supplemental_order_missing_payees`).flush();
+            return logger.warn('smart_button_validation_error_supplemental_order_missing_payees');
         }
 
         if (!payees.length) {
-            return getLogger().warn(`supplemental_order_payees_empty`).flush();
+            return logger.warn('smart_button_validation_error_supplemental_order_no_payees');
         }
 
         // find and remove duplicated payees
@@ -114,7 +119,7 @@ export function validateOrder(orderID : string, { env, clientID, merchantID, exp
 
         for (const payee of payees) {
             if (!payee.merchantId && (!payee.email || !payee.email.stringValue)) {
-                return getLogger().warn(`supplemental_order_payees_missing_value`, { payees: JSON.stringify(payees) }).flush();
+                return logger.warn('smart_button_validation_error_supplemental_order_missing_values', { payees: JSON.stringify(payees) });
             }
 
             if (payee.merchantId) {
@@ -139,6 +144,7 @@ export function validateOrder(orderID : string, { env, clientID, merchantID, exp
                 return payee.email.stringValue;
             }
 
+            logger.warn('smart_button_validation_error_invalid_payee_state', { uniquePayees: JSON.stringify(uniquePayees) });
             throw new Error(`Invalid payee state: ${ JSON.stringify(uniquePayees) }`);
         }).join(',');
 
@@ -148,7 +154,7 @@ export function validateOrder(orderID : string, { env, clientID, merchantID, exp
             
             // Validate merchant-id value(s) passed explicitly to SDK
             if (!isValidMerchantIDs(xpropMerchantID, uniquePayees)) {
-                getLogger().warn(`explicit_payee_transaction_mismatch`, { payees: JSON.stringify(uniquePayees), merchantID: JSON.stringify(merchantID) }).flush();
+                logger.warn(`smart_button_validation_error_explicit_payee_transaction_mismatch`, { payees: JSON.stringify(uniquePayees), merchantID: JSON.stringify(merchantID) });
 
                 if (uniquePayees.length === 1) {
                     throw new Error(`Payee(s) passed in transaction does not match expected merchant id. Please ensure you are passing ${ SDK_QUERY_KEYS.MERCHANT_ID }=${ payeesStr } or ${ SDK_QUERY_KEYS.MERCHANT_ID }=${ (uniquePayees[0] && uniquePayees[0].email && uniquePayees[0].email.stringValue) ? uniquePayees[0].email.stringValue : 'payee@merchant.com' } to the sdk url. https://developer.paypal.com/docs/checkout/reference/customize-sdk/`);
@@ -160,11 +166,11 @@ export function validateOrder(orderID : string, { env, clientID, merchantID, exp
 
             // Validate merchant-id value derived from client id
             if (!isValidMerchantIDs(merchantID, uniquePayees)) {
-                getLogger().warn(`derived_payee_transaction_mismatch`, { payees: JSON.stringify(uniquePayees), merchantID: JSON.stringify(merchantID) }).flush();
+                logger.warn(`smart_button_validation_error_derived_payee_transaction_mismatch`, { payees: JSON.stringify(uniquePayees), merchantID: JSON.stringify(merchantID) });
 
                 if (uniquePayees.length === 1) {
                     if (env === ENV.SANDBOX) {
-                        getLogger().warn(`derived_payee_transaction_mismatch_sandbox`, { payees: JSON.stringify(payees), merchantID: JSON.stringify(merchantID) }).flush();
+                        logger.warn(`smart_button_validation_error_derived_payee_transaction_mismatch_sandbox`, { payees: JSON.stringify(payees), merchantID: JSON.stringify(merchantID) });
                     }
 
                     // eslint-disable-next-line no-console
@@ -175,13 +181,16 @@ export function validateOrder(orderID : string, { env, clientID, merchantID, exp
             }
         }
 
+    }).then(() => {
+        logger.flush();
+
     }).catch(err => {
         const isSandbox = (env === ENV.SANDBOX);
         const isWhitelisted = isSandbox
             ? (clientID && SANDBOX_ORDER_VALIDATION_WHITELIST.indexOf(clientID) !== -1)
             : (clientID && ORDER_VALIDATION_WHITELIST.indexOf(clientID) !== -1);
 
-        getLogger()
+        logger
             .warn(`${ isSandbox ? 'sandbox_' : '' }order_validation_error${ isWhitelisted ? '_whitelist' : '' }`, { err: stringifyError(err) })
             .warn(`${ isSandbox ? 'sandbox_' : '' }order_validation_error${ isWhitelisted ? '_whitelist' : '' }_${ clientID || 'unknown' }`, { err: stringifyError(err) })
             .track({
