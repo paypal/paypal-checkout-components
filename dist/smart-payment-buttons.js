586f87c89bd1ba1b562a8cbc1d29001dbe804ca2
Daniel Brain <dbrain@paypal.com>
2020-09-03 15:42:54 -0700

    Dist

--- dist/smart-payment-buttons.js
+++ dist/smart-payment-buttons.js
@@ -1830,7 +1830,7 @@ window.spb = function(modules) {
         getLogger().info("rest_api_create_order_token");
         var headers = ((_headers10 = {}).authorization = "Bearer " + accessToken, _headers10["paypal-partner-attribution-id"] = partnerAttributionID, 
         _headers10["paypal-client-metadata-id"] = clientMetadataID, _headers10["x-app-name"] = "smart-payment-buttons", 
-        _headers10["x-app-version"] = "2.0.305", _headers10);
+        _headers10["x-app-version"] = "2.0.306", _headers10);
         var paymentSource = {
             token: {
                 id: paymentMethodID,
@@ -2104,7 +2104,7 @@ window.spb = function(modules) {
                     var data = {
                         orderID: orderID,
                         payerID: payerID,
-                        paymentID: paymentID,
+                        paymentID: paymentID = paymentID || supplementalData && supplementalData.checkoutSession && supplementalData.checkoutSession.cart && supplementalData.checkoutSession.cart.paymentId,
                         billingToken: billingToken = billingToken || supplementalData && supplementalData.checkoutSession && supplementalData.checkoutSession.cart && supplementalData.checkoutSession.cart.billingToken,
                         subscriptionID: subscriptionID,
                         facilitatorAccessToken: facilitatorAccessToken,
@@ -5708,7 +5708,7 @@ window.spb = function(modules) {
                 var _ref2;
                 return (_ref2 = {}).state_name = "smart_button", _ref2.context_type = "button_session_id", 
                 _ref2.context_id = buttonSessionID, _ref2.state_name = "smart_button", _ref2.button_session_id = buttonSessionID, 
-                _ref2.button_version = "2.0.305", _ref2.button_correlation_id = buttonCorrelationID, 
+                _ref2.button_version = "2.0.306", _ref2.button_correlation_id = buttonCorrelationID, 
                 _ref2;
             }));
             (function() {
