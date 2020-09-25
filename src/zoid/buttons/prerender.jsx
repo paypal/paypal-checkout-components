38efe9165f20ef19b7325d7541e705af6a6f5f03
Daniel Brain <dbrain@paypal.com>
2020-09-25 13:29:33 -0700

    Add logs for pre-render button click

--- src/zoid/buttons/prerender.jsx
+++ src/zoid/buttons/prerender.jsx
@@ -2,10 +2,11 @@
 /** @jsx node */
 
 import { node, dom, type ChildType } from 'jsx-pragmatic/src';
-import { FUNDING, CARD } from '@paypal/sdk-constants/src';
+import { FUNDING, CARD, FPTI_KEY } from '@paypal/sdk-constants/src';
 import { popup, supportsPopups, writeElementToWindow } from 'belter/src';
 import { assertSameDomain, type CrossDomainWindowType } from 'cross-domain-utils/src';
 import { SpinnerPage } from '@paypal/common-components/src';
+import { getLogger } from '@paypal/sdk-client/src';
 
 import { DEFAULT_POPUP_SIZE } from '../checkout';
 import { Buttons } from '../../ui';
@@ -24,6 +25,13 @@ type PrerenderedButtonsProps = {|
 export function PrerenderedButtons({ nonce, onRenderCheckout, props } : PrerenderedButtonsProps) : ChildType {
 
     const handleClick = (event, { fundingSource, card } : {| fundingSource : $Values<typeof FUNDING>, card : ?$Values<typeof CARD> |}) => {
+        getLogger().info('button_prerender_click').track({
+            [ FPTI_KEY.BUTTON_SESSION_UID ]: props.buttonSessionID,
+            [ FPTI_KEY.CONTEXT_TYPE ]:       'button_session_id',
+            [ FPTI_KEY.CONTEXT_ID ]:         props.buttonSessionID,
+            [ FPTI_KEY.TRANSITION ]:         'process_button_prerender_click'
+        }).flush();
+        
         if (supportsPopups()) {
             const win = assertSameDomain(popup('', {
                 width:  DEFAULT_POPUP_SIZE.WIDTH,
