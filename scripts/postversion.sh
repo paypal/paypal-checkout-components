0f77c3d225cf449eccabec322a8589099b70d9f2
Nate Bierdeman <nbierdeman@paypal.com>
2020-09-29 12:33:03 -0500

    verify new version has been published and then publish to cdnx

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -5,3 +5,12 @@ git push;
 git push --tags;
 git push git@github.com:paypal/paypal-checkout.git v4;
 npm publish;
+
+# verify new version has been published and then publish to cdnx
+local_version=$(node --eval "
+    const PACKAGE = './package.json';
+    let pkg = require(PACKAGE);
+    console.log(pkg.version);
+")
+npx grabthar-verify-npm-publish "$local_version" "latest"
+npm run cdnify
