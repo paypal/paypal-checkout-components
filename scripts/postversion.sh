155c6a92cdbc45d189798efdb2471c4c1ca020f4
Nate Bierdeman <natebierdeman@gmail.com>
2020-09-30 12:40:18 -0500

    Improve v4 release process with grabthar-release (#1443)
    * Add grabthar-release as dev dependency and cdnify command
    
    * verify new version has been published and then publish to cdnx
    
    * add .nodeops file
    
    * Update comment

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -5,3 +5,12 @@ git push;
 git push --tags;
 git push git@github.com:paypal/paypal-checkout.git v4;
 npm publish;
+
+# publish to cdnx after verifying a successful publish to the public npm registry
+local_version=$(node --eval "
+    const PACKAGE = './package.json';
+    let pkg = require(PACKAGE);
+    console.log(pkg.version);
+")
+npx grabthar-verify-npm-publish "$local_version" "latest"
+npm run cdnify
