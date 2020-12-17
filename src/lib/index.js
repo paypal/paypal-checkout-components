ba68efcc216935db2e54e0c902751d78ea811df4
Daniel Brain <dbrain@paypal.com>
2020-12-16 17:27:26 -0800

    Add localStorage session logic

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,3 +4,4 @@ export * from './util';
 export * from './logger';
 export * from './dom';
 export * from './comms';
+export * from './session';
---
+++ src/lib/session.js
@@ -0,0 +1,25 @@
+/* @flow */
+
+import { getStorage, type Storage } from 'belter/src';
+
+function getSDKStorage() : Storage {
+    return getStorage({
+        name: 'smart_payment_buttons'
+    });
+}
+
+export function getSessionID() : string {
+    return getSDKStorage().getSessionID();
+}
+
+export function getStorageState<T>(handler : (storage : Object) => T) : T {
+    return getSDKStorage().getState(handler);
+}
+
+export function getStorageID() : string {
+    return getSDKStorage().getID();
+}
+
+export function getSessionState<T>(handler : (state : Object) => T) : T {
+    return getSDKStorage().getSessionState(handler);
+}
