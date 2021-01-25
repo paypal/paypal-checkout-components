1d8223107a1ebf3380daab73fcaf032596ad6399
Daniel Brain <dbrain@paypal.com>
2021-01-25 11:35:21 -0800

    Dist

--- dist/smart-payment-buttons.js
+++ dist/smart-payment-buttons.js
@@ -1683,7 +1683,7 @@ window.spb = function(modules) {
             logger_getLogger().info("rest_api_create_order_token");
             var headers = ((_headers10 = {}).authorization = "Bearer " + accessToken, _headers10["paypal-partner-attribution-id"] = partnerAttributionID, 
             _headers10["paypal-client-metadata-id"] = clientMetadataID, _headers10["x-app-name"] = "smart-payment-buttons", 
-            _headers10["x-app-version"] = "2.0.358", _headers10);
+            _headers10["x-app-version"] = "2.0.359", _headers10);
             var paymentSource = {
                 token: {
                     id: paymentMethodID,
@@ -5266,7 +5266,7 @@ window.spb = function(modules) {
                                                 _getLogger$info$track16.state_name = "smart_button", _getLogger$info$track16.transition_name = "popup_closed", 
                                                 _getLogger$info$track16)).flush();
                                                 return promise_ZalgoPromise.delay(1e3).then((function() {
-                                                    if (!approved && !cancelled && !didFallback) return promise_ZalgoPromise.all([ onCancel(), close() ]);
+                                                    if (!(approved || cancelled || didFallback || isAndroidChrome())) return promise_ZalgoPromise.all([ onCancel(), close() ]);
                                                 })).then(src_util_noop);
                                             }();
                                         }
@@ -6124,7 +6124,7 @@ window.spb = function(modules) {
                     var _ref2;
                     return (_ref2 = {}).state_name = "smart_button", _ref2.context_type = "button_session_id", 
                     _ref2.context_id = buttonSessionID, _ref2.state_name = "smart_button", _ref2.button_session_id = buttonSessionID, 
-                    _ref2.button_version = "2.0.358", _ref2.button_correlation_id = buttonCorrelationID, 
+                    _ref2.button_version = "2.0.359", _ref2.button_correlation_id = buttonCorrelationID, 
                     _ref2.stickiness_id = stickinessID, _ref2.bn_code = partnerAttributionID, _ref2.user_action = commit ? "commit" : "continue", 
                     _ref2.seller_id = merchantID[0], _ref2.merchant_domain = merchantDomain, _ref2.t = Date.now().toString(), 
                     _ref2;
