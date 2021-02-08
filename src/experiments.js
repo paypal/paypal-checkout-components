3383ad508cef80098ab67645da5232d03634e3e4
Daniel Brain <dbrain@paypal.com>
2021-02-08 12:54:44 -0800

    Ramp native fakeout experiment to 10%

--- src/experiments.js
+++ src/experiments.js
@@ -4,4 +4,4 @@ import { type Experiment } from 'belter/src';
 
 import { createExperiment } from './lib';
 
-export const nativeFakeoutExperiment : Experiment = createExperiment('native_popup_fakeout', 0);
+export const nativeFakeoutExperiment : Experiment = createExperiment('native_popup_fakeout', 10);
