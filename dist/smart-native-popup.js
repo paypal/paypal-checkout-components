956d7b044bd8e416525e7822f6a1c6363bd64fe0
Daniel Brain <dbrain@paypal.com>
2021-02-24 15:45:50 -0800

    Dist

--- dist/smart-native-popup.js
+++ dist/smart-native-popup.js
@@ -1183,6 +1183,11 @@
                     locale: locale,
                     sdkVersion: sdkVersion
                 });
+                logger.addMetaBuilder((function() {
+                    return {
+                        amplitude: !0
+                    };
+                }));
                 logger.addPayloadBuilder((function() {
                     var _ref2;
                     return (_ref2 = {
@@ -1193,7 +1198,7 @@
                     var _ref3;
                     return (_ref3 = {}).state_name = "smart_button", _ref3.context_type = "button_session_id", 
                     _ref3.context_id = buttonSessionID, _ref3.state_name = "smart_button", _ref3.button_session_id = buttonSessionID, 
-                    _ref3.button_version = "2.0.379", _ref3.user_id = buttonSessionID, _ref3;
+                    _ref3.button_version = "2.0.380", _ref3.user_id = buttonSessionID, _ref3;
                 }));
                 (function() {
                     if (window.document.documentMode) try {
