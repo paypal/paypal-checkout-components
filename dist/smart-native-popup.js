8d4b8aeafbec066141a98f3ffbf5cb0171179282
Steve Mask <smask@paypal.com>
2021-03-05 16:16:05 -0500

    Dist

--- dist/smart-native-popup.js
+++ dist/smart-native-popup.js
@@ -1422,7 +1422,7 @@
                     var _ref3;
                     return (_ref3 = {}).state_name = "smart_button", _ref3.context_type = "button_session_id", 
                     _ref3.context_id = buttonSessionID, _ref3.state_name = "smart_button", _ref3.button_session_id = buttonSessionID, 
-                    _ref3.button_version = "5.0.5", _ref3.user_id = buttonSessionID, _ref3;
+                    _ref3.button_version = "5.0.6", _ref3.user_id = buttonSessionID, _ref3;
                 }));
                 (function() {
                     if (window.document.documentMode) try {
