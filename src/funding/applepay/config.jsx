b3cb991422ccc0a8f8e59ca1ed62ccba5e1846d7
Steve Mask <smask@paypal.com>
2021-03-22 18:37:56 -0400

    add secondary color override

--- src/funding/applepay/config.jsx
+++ src/funding/applepay/config.jsx
@@ -32,6 +32,12 @@ export function getApplePayConfig() : FundingSourceConfig {
             BUTTON_COLOR.WHITE
         ],
 
+        secondaryColors: {
+            ...DEFAULT_FUNDING_CONFIG.secondaryColors,
+
+            [ BUTTON_COLOR.DEFAULT ]:   BUTTON_COLOR.BLACK
+        },
+
         logoColors:  {
             [ BUTTON_COLOR.BLACK ]:  LOGO_COLOR.WHITE,
             [ BUTTON_COLOR.WHITE ]:  LOGO_COLOR.BLACK
