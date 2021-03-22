16dbe98c2fb6ecdbfa8aebff18394cdc76afb947
Steve Mask <smask@paypal.com>
2021-03-22 15:12:19 -0400

    lint

--- src/funding/applepay/config.jsx
+++ src/funding/applepay/config.jsx
@@ -2,10 +2,9 @@
 /** @jsx node */
 
 import { PLATFORM } from '@paypal/sdk-constants/src';
-import { getUserAgent, isIOS, isSafari } from 'belter';
 import { ApplePayLogo, LOGO_COLOR } from '@paypal/sdk-logos/src';
 
-import { BUTTON_COLOR, BUTTON_LAYOUT } from '../../constants';
+import { BUTTON_COLOR, BUTTON_SHAPE, BUTTON_LAYOUT } from '../../constants';
 import { DEFAULT_FUNDING_CONFIG, type FundingSourceConfig } from '../common';
 
 export function getApplePayConfig() : FundingSourceConfig {
