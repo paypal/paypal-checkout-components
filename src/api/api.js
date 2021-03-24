bb5bbfb636f5031ba5d9ed914e2063fe3c1037c3
Andy Fenelon <5710962+andyfen@users.noreply.github.com>
2021-03-24 12:16:56 -0700

    fix typo in details (#140)
    Co-authored-by: andy <xxxandyfenxxx@xxx.com>

--- src/api/api.js
+++ src/api/api.js
@@ -34,7 +34,11 @@ export function callRestAPI<D, T>({ accessToken, method, url, data, headers } : 
         json:    data
     }).then(({ status, body, headers: responseHeaders }) : T => {
         if (status >= 300) {
-            throw new Error(`${ body.detail[0].issue }: ${ body.detail[0].description } (Corr ID: ${ responseHeaders[HEADERS.PAYPAL_DEBUG_ID] }`);
+            const hasDetails = body.details && body.details.length;
+            const issue = (hasDetails && body.details[0].issue) ? body.details[0].issue : 'Generic Error';
+            const description = (hasDetails && body.details[0].description) ? body.details[0].description : 'no description';
+            
+            throw new Error(`${ issue }: ${ description } (Corr ID: ${ responseHeaders[HEADERS.PAYPAL_DEBUG_ID] }`);
         }
 
         return body;
