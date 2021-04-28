ef4eca8707a14342f2fe14e7a77ccf9cea2830ab
Daniel Brain <dbrain@paypal.com>
2021-04-28 13:42:54 -0700

    Test fix

--- test/integration/windows/fields/index.js
+++ test/integration/windows/fields/index.js
@@ -1,10 +1,5 @@
 /* @flow */
-import { ZalgoPromise } from 'zalgo-promise/src';
 
-window.xprops.payment().then(() => {
-    return ZalgoPromise.try(() => {
-        if (window.xprops.onInit) {
-            window.xprops.onInit();
-        }
-    });
-});
+if (window.xprops.onInit) {
+    window.xprops.onInit();
+}
