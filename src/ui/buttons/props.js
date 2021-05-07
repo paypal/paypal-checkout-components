baf1cf9343f1345ffa83a2286b8d79a75158b964
Steve Mask <smask@paypal.com>
2021-05-06 22:31:32 -0400

    update applepaysession

--- src/ui/buttons/props.js
+++ src/ui/buttons/props.js
@@ -213,7 +213,7 @@ export type ApplePayPaymentAuthorizationResult = {|
 
 export type ApplePaySessionConfig = {|
     begin : () => void,
-    addEventListener : (string, Function) => void,
+    addListener : (string, Function) => void,
     // eslint-disable-next-line flowtype/no-weak-types
     completeMerchantValidation : (validatedSession : any) => void,
     completeShippingMethodSelection : (update : ApplePayShippingMethodUpdate | {||}) => void,
