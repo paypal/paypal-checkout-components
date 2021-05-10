53869873ccded15a33e0f54d4115b828302b9e91
Steve Mask <smask@paypal.com>
2021-05-10 11:52:29 -0400

    test

--- test/integration/tests/button/props.js
+++ test/integration/tests/button/props.js
@@ -5,7 +5,7 @@ import { wrapPromise } from 'belter/src';
 import { FUNDING } from '@paypal/sdk-constants/src';
 import { ZalgoPromise } from 'zalgo-promise/src';
 
-import { createTestContainer, destroyTestContainer } from '../common';
+import { createTestContainer, destroyTestContainer, IPHONE6_USER_AGENT } from '../common';
 
 describe(`paypal button component props`, () => {
 
@@ -15,6 +15,77 @@ describe(`paypal button component props`, () => {
 
     afterEach(() => {
         destroyTestContainer();
+    });
+
+    it.only('should render an Apple Pay button if applePaySupport is true', () => {
+        // setup applePaySupport
+        window.navigator.mockUserAgent = IPHONE6_USER_AGENT;
+        window.ApplePaySession = {
+            canMakePayments: () => true,
+            supportsVersion: () => true
+        };
+
+        return ZalgoPromise.try(() => {
+            return wrapPromise(({ expect, avoid }) => {
+                let onRender = ({ xprops }) => {
+                    const applePay = xprops.applePay;
+                    const request = {
+                        'countryCode':          'US',
+                        'currencyCode':         'USD',
+                        'merchantCapabilities': [
+                            'supports3DS'
+                        ],
+                        'supportedNetworks': [
+                            'visa',
+                            'masterCard',
+                            'amex',
+                            'discover'
+                        ],
+                        'total': {
+                            'label':    'Demo (Card is not charged)',
+                            'type':     'final',
+                            'amount':   '1.99'
+                        }
+                    };
+                    applePay(3, request).then(response => {
+                        const {
+                            begin,
+                            addListener,
+                            completeMerchantValidation,
+                            completeShippingContactSelection,
+                            completePaymentMethodSelection,
+                            completeShippingMethodSelection,
+                            completePayment
+                        } = response;
+
+                        expect(begin);
+                        expect(addListener);
+                        expect(completeMerchantValidation);
+                        expect(completeShippingContactSelection);
+                        expect(completePaymentMethodSelection);
+                        expect(completeShippingMethodSelection);
+                        expect(completePayment);
+                    });
+                };
+
+                const fundingSource = FUNDING.APPLEPAY;
+                const instance = window.paypal.Buttons({
+                    test: {
+                        action:   'checkout',
+                        onRender: (...args) => onRender(...args)
+                    },
+                    fundingSource,
+                    onApprove: avoid('onApprove'),
+                    onCancel:  avoid('onCancel')
+
+                });
+                
+                if (instance.isEligible()) {
+                    onRender = expect('onRender', onRender);
+                    return instance.render('#testContainer');
+                }
+            });
+        });
     });
 
     it('should render a button and get any queried FIs', () => {
@@ -28,34 +99,6 @@ describe(`paypal button component props`, () => {
         return ZalgoPromise.all(fundingSources.map(fundingSource => {
             return wrapPromise(({ expect, avoid }) => {
                 let onRender = ({ xprops }) => {
-                    if (fundingSource === FUNDING.APPLEPAY) {
-                        window.ApplePaySession = {
-                            canMakePayments: () => true
-                        };
-                        const applePay = xprops.applePay();
-                        const request = {
-                            'countryCode':          'US',
-                            'currencyCode':         'USD',
-                            'merchantCapabilities': [
-                                'supports3DS'
-                            ],
-                            'supportedNetworks': [
-                                'visa',
-                                'masterCard',
-                                'amex',
-                                'discover'
-                            ],
-                            'total': {
-                                'label':    'Demo (Card is not charged)',
-                                'type':     'final',
-                                'amount':   '1.99'
-                            }
-                        };
-                        const session = applePay ? applePay(3, request) : undefined;
-                        if (session) {
-                            session.begin();
-                        }
-                    }
                     return xprops.getQueriedEligibleFunding().then(queriedFundingSources => {
                         if (JSON.stringify(queriedFundingSources) !== JSON.stringify(fundingSources)) {
                             throw new Error(`Expected ${ fundingSources.join(',') } to be queried, got ${ queriedFundingSources.join(',') }`);
