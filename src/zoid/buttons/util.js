b7338249580ed856fdac7dc1cb8475f5883f7627
Steve Mask <smask@paypal.com>
2021-05-24 13:01:47 -0400

    flow fix

--- src/zoid/buttons/util.js
+++ src/zoid/buttons/util.js
@@ -82,6 +82,7 @@ export function applePaySession() : ?ApplePaySessionConfigRequest {
 
         const convertErrorsFromUpdate = (update) => {
             if (update.errors && update.errors.length) {
+                // $FlowFixMe
                 return update.errors.map(error => new window.ApplePayError(error.code, error.contactField, error.message));
             }
 
