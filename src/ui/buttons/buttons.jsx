ff5a931a14720ab5f22f2287ef0a9d53ba80ec91
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2021-06-22 15:20:06 -0500

    Pass csp nonce to PoweredByPayPal component (#1668)

--- src/ui/buttons/buttons.jsx
+++ src/ui/buttons/buttons.jsx
@@ -182,6 +182,7 @@ export function Buttons(props : ButtonsProps) : ElementNode {
                 (layout === BUTTON_LAYOUT.VERTICAL && fundingSources.indexOf(FUNDING.CARD) !== -1)
                     ? <PoweredByPayPal
                         locale={ locale }
+                        nonce={ nonce }
                     /> : null
             }
 
