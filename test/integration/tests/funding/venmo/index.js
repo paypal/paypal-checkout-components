440e680f0312f1939eca867079414122936d234e
Brice Shatzer <brice.shatzer@gmail.com>
2021-06-29 14:39:32 -0500

    updated desktop user agent

--- test/integration/tests/funding/venmo/index.js
+++ test/integration/tests/funding/venmo/index.js
@@ -2,7 +2,7 @@
 
 import { FUNDING } from '@paypal/sdk-constants/src';
 
-import { IPHONE6_USER_AGENT, WEBVIEW_USER_AGENT, createTestContainer, destroyTestContainer, mockProp, assert, getElementRecursive } from '../../common';
+import { IPHONE6_USER_AGENT, COMMON_DESKTOP_USER_AGENT, createTestContainer, destroyTestContainer, mockProp, assert, getElementRecursive } from '../../common';
 
 const fundingSource = FUNDING.VENMO;
 
@@ -68,7 +68,7 @@ describe(`venmo desktop web button test `, () => {
 
     beforeEach(() => {
         createTestContainer();
-        window.navigator.mockUserAgent = WEBVIEW_USER_AGENT;
+        window.navigator.mockUserAgent = COMMON_DESKTOP_USER_AGENT;
         
     });
 
@@ -77,7 +77,7 @@ describe(`venmo desktop web button test `, () => {
     });
 
 
-    it(`should not display button when it's passed in as instrument on desktop web`, (done) => {
+    it.only(`should not display button when it's passed in as instrument on desktop web`, (done) => {
         mockProp(window.__TEST_FUNDING_ELIGIBILITY__[fundingSource], 'eligible', true);
         
         const paypalButtons = window.paypal.Buttons({
