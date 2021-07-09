51252388e7158d82ee50ca330a5516f7f59f822e
Alan Bendjuya <abendjuya@paypal.com>
2021-07-09 15:41:27 -0300

    Make label for buttons smaller only for German locale and in specific widths

--- src/ui/buttons/style.jsx
+++ src/ui/buttons/style.jsx
@@ -2,19 +2,21 @@
 /** @jsx node */
 
 import { node, type ElementNode } from 'jsx-pragmatic/src';
+import { type LocaleType } from '@paypal/sdk-constants/src';
 
 import { type ButtonStyle } from './props';
 import { componentStyle } from './styles';
 
 type StyleProps = {|
     style : ButtonStyle,
-    nonce : string
+    nonce : string,
+    locale : LocaleType
 |};
 
-export function Style({ style, nonce } : StyleProps) : ElementNode {
+export function Style({ style, nonce, locale } : StyleProps) : ElementNode {
 
     const { height } = style;
-    const css = componentStyle({ height });
+    const css = componentStyle({ height, locale });
 
     return (
         <style nonce={ nonce } innerHTML={ css } />
