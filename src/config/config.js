016c2473c8c0eb057796949b07847844f1ca2d54
Steve Mask <smask@paypal.com>
2021-07-19 16:55:13 -0400

    update lint

--- src/config/config.js
+++ src/config/config.js
@@ -684,11 +684,11 @@ export const config = {
 
     SUPPORTED_BROWSERS: {
         msie:           '11',
-        firefox:        '30',
-        chrome:         '27',
-        safari:         '7',
-        opera:          '16',
-        msedge:         '12',
+        firefox:        '43',
+        chrome:         '41',
+        safari:         '8',
+        opera:          '23',
+        msedge:         '14',
         samsungBrowser: '2.1',
         silk:           '59.3',
         ucbrowser:      '10.0.0.488',
