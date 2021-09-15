7e45033ea37e0728ba19c6a54754ded66246cea0
amarri <amarri@paypal.com>
2021-09-15 13:00:34 -0700

    fixing tests for dimensions function

--- test/integration/tests/checkout/dimensions.js
+++ test/integration/tests/checkout/dimensions.js
@@ -1,11 +1,11 @@
 /* @flow */
 /* eslint max-lines: 0 */
 
-import { wrapPromise, uniqueID, getElement } from 'belter/src';
+import { uniqueID, noop } from 'belter/src';
 import { FUNDING } from '@paypal/sdk-constants/src';
 
 import { generateOrderID, runOnClick,
-    createTestContainer, destroyTestContainer, assert
+    createTestContainer, destroyTestContainer, onWindowOpen
 } from '../common';
 
 describe(`paypal checkout component happy path with dimensions`, () => {
@@ -18,26 +18,36 @@ describe(`paypal checkout component happy path with dimensions`, () => {
         destroyTestContainer();
     });
 
-    it('should render checkout, then complete the payment with dimensions', () => {
-        return wrapPromise(({ expect, error }) => {
-            return runOnClick(() => {
-                return window.paypal.Checkout({
-                    buttonSessionID: uniqueID(),
-                    dimensions:      { width: '1282', height: '720' },
-                    fundingSource:   FUNDING.PAYPAL,
-                    createOrder:     expect('createOrder', generateOrderID),
-                    onApprove:       expect('onApprove'),
-                    onCancel:        error('onCancel')
-                }).render('body').then(() => {
-                    // eslint-disable-next-line max-nested-callbacks
-                    setTimeout(() => {
-                        const e = getElement('#body').getBoundingClientRect();
-                        const { width, height } = e;
-                        assert.ok(height === '720');
-                        assert.ok(width === '1282');
-                    }, 100);
-                });
+    it('should render checkout, then complete the payment with dimensions', (done) => {
+        const APM_DEFAULT_POPUP_SIZE = {
+            WIDTH:  1282,
+            HEIGHT: 720
+        };
+
+        runOnClick(() => {
+            let childWindow;
+            onWindowOpen().then(win => {
+                childWindow = win;
+                // $FlowFixMe
+                const { outerWidth: width, outerHeight: height } = childWindow;
+
+                if (height !== APM_DEFAULT_POPUP_SIZE.HEIGHT) {
+                    done(`height does not match. expected ${ APM_DEFAULT_POPUP_SIZE.HEIGHT }, got ${ height }`);
+                }
+                if (width !== APM_DEFAULT_POPUP_SIZE.WIDTH) {
+                    done(`width does not match. expected ${ APM_DEFAULT_POPUP_SIZE.WIDTH }, got ${ width }`);
+                }
+                done();
             });
+
+            return window.paypal.Checkout({
+                test:            { action: 'init' },
+                dimensions:      { width: '1282', height: '720' },
+                buttonSessionID: uniqueID(),
+                fundingSource:   FUNDING.PAYPAL,
+                createOrder:     generateOrderID,
+                onApprove:       noop
+            }).render('body');
         });
     });
 });
