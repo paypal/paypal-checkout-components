8c06d7e5b865ae28f5ce3b94e5811e8e528d973c
erick <erickgarcia@paypal.com>
2021-11-02 17:56:41 -0500

    remove extra comma

--- src/ui/buttons/button-animations/types.js
+++ src/ui/buttons/button-animations/types.js
@@ -43,7 +43,7 @@ export type LabelOptions = {|
 
 export type DivideLogoAnimationProps = {|
     paypalLabelContainerElement : ElementNode,
-    paypalLogoStartingLeftPosition : string,
+    paypalLogoStartingLeftPosition : string
 |};
 
 export type AnimationProps = {|
