f44d1b3387675f02d2a21894ceaafc755484a743
erick garcia <erickgarcia@paypal.com>
2021-12-06 09:01:24 -0500

    add tests for one time animation

--- test/ssr/ssr.test.js
+++ test/ssr/ssr.test.js
@@ -301,6 +301,40 @@ test(`Design for switch logo and show label text should be applied when there is
     }
 });
 
+test(`Design for switch logo and show label text one time should be applied when there is valid personalization`, async () => {
+    const { Buttons } = await getButtonScript();
+
+    const personalization = {
+        buttonDesign: {
+            id:       'run-switch-logo-show-label-text-once-design',
+            text:     'Safe and easy way to pay',
+            tracking: {
+                click:      '',
+                impression: ''
+            }
+        }
+    };
+
+    const buttonHTML = Buttons({
+        locale:          { country: 'US', lang: 'en' },
+        platform:        'desktop',
+        sessionID:       'xyz',
+        buttonSessionID: 'abc',
+        personalization,
+        fundingEligibility
+    }).render(html());
+
+    if (!buttonHTML || typeof buttonHTML !== 'string') {
+        throw new Error(`Expected html to be a non-empty string`);
+    }
+    const designContainer = buttonHTML.match('data-design-experiment');
+    const designScript = buttonHTML.match('.personalized-design-container');
+
+    if (!designContainer || !designScript) {
+        throw new Error('Expected design to be applied in script and container');
+    }
+});
+
 test(`Tag should be applied with no design for control`, async () => {
 
     const { Buttons } = await getButtonScript();
