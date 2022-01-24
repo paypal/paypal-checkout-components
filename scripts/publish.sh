67ca54645c55bcf072315857ac4a6b7a195b609c
Nate Bierdeman <nbierdeman@paypal.com>
2022-01-24 16:47:30 -0700

    Export release variable for use with npm publish --tag in postversion.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
 # determine patch or alpha release based on current branch
-release=patch
+release='patch'
 default_branch=$( git remote show origin | sed -n '/HEAD branch/s/.*: //p' )
 current_branch=$( git rev-parse --abbrev-ref HEAD )
 
 if [ "$current_branch" != "$default_branch" ]
 then
-  release='prerelease --preid=alpha'
+  release='alpha'
 fi
 
 # Running `npm version` will run through the other version lifecycle scripts
-# npm version $release;
+
+if [ "$release" = 'patch' ]
+then
+  npm version $release
+else
+  npm version prerelease --preid=$release
+fi
+
+export release
