92b61a4b23bad9eeb4504105ce065fd0f2872b6f
Nate Bierdeman <nbierdeman@paypal.com>
2022-01-24 15:42:11 -0700

    Determine patch or alpha release based on current branch

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+# determine patch or alpha release based on current branch
+release=patch
+default_branch=$( git remote show origin | sed -n '/HEAD branch/s/.*: //p' )
+current_branch=$( git rev-parse --abbrev-ref HEAD )
+
+if [ "$current_branch" != "$default_branch" ]
+then
+  release=prerelease
+fi
+
 # Running `npm version` will run through the other version lifecycle scripts
-npm version ${1-patch};
+# npm version $release;
