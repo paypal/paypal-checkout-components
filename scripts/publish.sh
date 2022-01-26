77770c46d7f66f1f6dc583801382c913be14272c
Nate Bierdeman <nbierdeman@paypal.com>
2022-01-26 09:17:47 -0700

    Accept command line argument for major and minor releases

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # determine patch or alpha release based on current branch, then run through the other version lifecycle scripts
-release='patch';
+release=${1-'patch'};
+
 current_branch=$( git rev-parse --abbrev-ref HEAD );
 default_branch=$( git remote show origin | sed -n '/HEAD branch/s/.*: //p' );
 
