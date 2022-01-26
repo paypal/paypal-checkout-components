a65f92e15c21f1c3b1c03c440d9531fa3e60b95d
Nate Bierdeman <nbierdeman@paypal.com>
2022-01-26 13:09:13 -0700

    Revert: Accept command line argument for major and minor releases

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Determine the type of release, then run through the other version lifecycle scripts
-release=${1-'patch'};
+release='patch';
 
 current_branch=$( git rev-parse --abbrev-ref HEAD );
 default_branch=$( git remote show origin | sed -n '/HEAD branch/s/.*: //p' );
