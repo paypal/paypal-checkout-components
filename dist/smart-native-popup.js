a9e194aad08ef35b528b386b1e21534bef56c6a7
Nate Bierdeman <nbierdeman@paypal.com>
2022-03-04 09:44:33 -0700

    Dist

--- dist/smart-native-popup.js
+++ dist/smart-native-popup.js
@@ -4888,7 +4888,7 @@
                 logger.addTrackingBuilder((function() {
                     var _ref3;
                     return (_ref3 = {}).state_name = "smart_button", _ref3.context_type = "button_session_id", 
-                    _ref3.context_id = buttonSessionID, _ref3.button_session_id = buttonSessionID, _ref3.button_version = "5.0.83", 
+                    _ref3.context_id = buttonSessionID, _ref3.button_session_id = buttonSessionID, _ref3.button_version = "5.0.84", 
                     _ref3.user_id = buttonSessionID, _ref3.time = Date.now().toString(), _ref3;
                 }));
                 (function() {
