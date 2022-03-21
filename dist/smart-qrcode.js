8c9234dfa2c3d9d7a6f57b7be30cbe6606de13ab
elizabethmv <elizabethmv@users.noreply.github.com>
2022-03-21 16:17:57 +0000

    Dist

--- dist/smart-qrcode.js
+++ dist/smart-qrcode.js
@@ -3165,7 +3165,7 @@
             logger.addTrackingBuilder((function() {
                 var _ref2;
                 return (_ref2 = {}).state_name = "smart_button", _ref2.context_type = "EC-Token", 
-                _ref2.context_id = orderID, _ref2.button_session_id = buttonSessionID, _ref2.button_version = "5.0.88", 
+                _ref2.context_id = orderID, _ref2.button_session_id = buttonSessionID, _ref2.button_version = "5.0.89", 
                 _ref2.user_id = buttonSessionID, _ref2;
             }));
             (function() {
