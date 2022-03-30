da4db3ec86940e1f17f489845426f26002da1618
Steve Mask <smask@paypal.com>
2022-03-30 12:09:35 -0400

    lint

--- src/zoid/buttons/component.jsx
+++ src/zoid/buttons/component.jsx
@@ -20,7 +20,7 @@ import { isFundingEligible } from '../../funding';
 import { containerTemplate } from './container';
 import { PrerenderedButtons } from './prerender';
 import { applePaySession, determineFlow, isSupportedNativeBrowser, createVenmoExperiment,
-    createNoPaylaterExperiment, getRenderedButtons, getButtonSize, getButtonExperiments, isInlineEligible } from './util';
+    createNoPaylaterExperiment, getRenderedButtons, getButtonSize, getButtonExperiments } from './util';
 
 export type ButtonsComponent = ZoidComponent<ButtonProps>;
 
