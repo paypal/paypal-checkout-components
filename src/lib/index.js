8b779250ad76305e95c0a2d1901b0839d1f037d5
Ruben <rfigueredo@paypal.com>
2022-04-25 13:10:20 -0400

    Refactored CPL names

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,4 +4,4 @@ export * from './errors';
 export * from './isRTLLanguage';
 export * from './security';
 export * from './session';
-export * from './perceivedLatencyInstrumentation';
+export * from './perceived-latency-instrumentation';
