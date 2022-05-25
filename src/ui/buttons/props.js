e9e3d07f7923735192bbcd0ea45f125e3300f43c
Steve Mask <smask@paypal.com>
2022-05-25 12:08:22 -0400

    types

--- src/ui/buttons/props.js
+++ src/ui/buttons/props.js
@@ -91,6 +91,56 @@ type OnShippingChangeOption = {|
     |}
 |};
 
+export type ON_SHIPPING_CHANGE_EVENT = 'add' | 'replace';
+
+export type ShippingOption = {|
+    id? : string,
+    label : string,
+    selected : boolean,
+    type : string,
+    amount : {|
+        currency_code : string,
+        value : string
+    |}
+|};
+
+export type Query = {|
+    op : ON_SHIPPING_CHANGE_EVENT,
+    path : string,
+    value : mixed
+|};
+
+export type Breakdown = {|
+    item_total? : {|
+        currency_code : $Values<typeof CURRENCY>,
+        value : string
+    |},
+    shipping? : {|
+        currency_code : $Values<typeof CURRENCY>,
+        value : string
+    |},
+    handling? : {|
+        currency_code : $Values<typeof CURRENCY>,
+        value : string
+    |},
+    tax_total? : {|
+        currency_code : $Values<typeof CURRENCY>,
+        value : string
+    |},
+    insurance? : {|
+        currency_code : $Values<typeof CURRENCY>,
+        value : string
+    |},
+    shipping_discount? : {|
+        currency_code : $Values<typeof CURRENCY>,
+        value : string
+    |},
+    discount? : {|
+        currency_code : $Values<typeof CURRENCY>,
+        value : string
+    |}
+|};
+
 export type ShippingAmount = {|
     breakdown? : {|
         item_total? : {|
@@ -143,19 +193,6 @@ export type OnShippingChangeActions = {|
 
 export type OnShippingChange = (data : OnShippingChangeData, actions : OnShippingChangeActions) => ZalgoPromise<void> | void;
 
-export type ShippingOption = {|
-    id? : string,
-    label : string,
-    selected : boolean,
-    type : string,
-    amount : {|
-        currency_code : string,
-        value : string
-    |}
-|};
-
-export type ON_SHIPPING_CHANGE_EVENT = 'add' | 'replace';
-
 export type OnShippingAddressChangeData = {|
     orderID : string,
     payerID? : string,
@@ -165,11 +202,6 @@ export type OnShippingAddressChangeData = {|
     shipping_address : OnShippingChangeAddress
 |};
 
-export type Query = {|
-    op : ON_SHIPPING_CHANGE_EVENT,
-    path : string,
-    value : {||}
-|};
 export type OnShippingAddressChangeActions = {|
     patch : () => ZalgoPromise<OrderGetResponse>,
     query : () => $ReadOnlyArray<Query>,
