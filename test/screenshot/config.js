2ae5cf3167d04f0cd7dab8b763c25d4dabcadd92
Nate Bierdeman <nbierdeman@paypal.com>
2022-11-29 11:45:12 -0700

    Add balance + bank screenshot

--- test/screenshot/config.js
+++ test/screenshot/config.js
@@ -1290,6 +1290,43 @@ buttonConfigs.push({
                     logoUrl: null,
                     oneClick: true,
                     planID: null,
+                    secondaryInstruments: [{
+                        instrumentID: "BALANCEUSD",
+                        label: "PayPal Balance",
+                        type: "BALANCE"
+                    }],
+                    tokenID: null,
+                    type:    'bank',
+                    vendor:  'BANK OF AMERICA'
+                }
+            ]
+        }
+    },
+    fundingEligibility: {
+        paypal: {
+            eligible: true
+        }
+    },
+    button: {
+        showPayLabel: false
+    }
+});
+
+buttonConfigs.push({
+    diffThreshold: 1000,
+    container:     {
+        width: 350
+    },
+    wallet: {
+        paypal: {
+            instruments: [
+                {
+                    accessToken: null,
+                    instrumentID: 'abc12345',
+                    label: '••1234',
+                    logoUrl: null,
+                    oneClick: true,
+                    planID: null,
                     secondaryInstruments: null,
                     tokenID: null,
                     type:    'bank',
