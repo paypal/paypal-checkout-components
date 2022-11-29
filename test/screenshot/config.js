521d5f79b1850eb553b2bd9886f4bd8d89ecf5ea
Nate Bierdeman <nbierdeman@paypal.com>
2022-11-29 11:23:35 -0700

    Add balance only screenshot

--- test/screenshot/config.js
+++ test/screenshot/config.js
@@ -1340,3 +1340,36 @@ buttonConfigs.push({
         showPayLabel: false
     }
 });
+
+buttonConfigs.push({
+    diffThreshold: 1000,
+    container:     {
+        width: 350
+    },
+    wallet: {
+        paypal: {
+            instruments: [
+                {
+                    accessToken: null,
+                    instrumentID: 'BALANCEUSD',
+                    label: 'BALANCE',
+                    logoUrl: null,
+                    oneClick: true,
+                    planID: null,
+                    secondaryInstruments: null,
+                    tokenID: null,
+                    type:    'balance',
+                    vendor:  'BALANCE'
+                }
+            ]
+        }
+    },
+    fundingEligibility: {
+        paypal: {
+            eligible: true
+        }
+    },
+    button: {
+        showPayLabel: false
+    },
+});
