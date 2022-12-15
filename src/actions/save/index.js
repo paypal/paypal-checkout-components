9ee34eceea604b866ab66d967c6e8eed61f9a497
Charlie Dibble <cgdibble@gmail.com>
2022-12-15 11:13:54 -0800

    feat: save action validates inputs (#2046)
    * setup vitest for unit testing, and validate save config inputs
    
    * add unit tests to main test run too
    
    Co-authored-by: Shane Brunson <brunson.ws@gmail.com>
    
    * fix lint issues and clean up
    
    * add vitest setup from spb
    
    * fix some last typing issues
    
    Co-authored-by: Shane Brunson <brunson.ws@gmail.com>

--- src/actions/save/index.js
+++ src/actions/save/index.js
@@ -1,17 +1,60 @@
 /* @flow */
 
-type SaveActionHandlers = {|
-  // eslint-disable-next-line no-restricted-globals, promise/no-native
-  onCreateVaultToken: () => Promise<string>,
-  onApprove: ({| token: string |}) => void,
+import { ZalgoPromise } from '@krakenjs/zalgo-promise/src';
+
+import { ValidationError } from "../../lib"
+
+type CardDetails = {|
+  number: ?string,
+  expiry: ?string,
+  name?: ?string,
+  security_code?: ?string,
+  type?: string,
+  postalCode?: ?string,
+  billingAddress? : string,
 |};
 
-export type SaveAction = (SaveActionHandlers) => ({|
+type SaveActionConfig = {|
+  createVaultSetupToken: () => ZalgoPromise<string>,
+  onApprove: ({| vaultSetupToken: string |}) => void,
+|};
+
+export type SaveAction = (SaveActionConfig) => ({|
   type: "save",
-  ...SaveActionHandlers,
+  /* TODO: 
+      - We need to define how paymentSourceDetails is typed here
+  */
+  save: ({|onError: (error: string) => void, paymentSourceDetails: CardDetails|}) => ZalgoPromise<void>,
 |});
 
-export const createSaveAction: SaveAction = (handlers: SaveActionHandlers) => ({
-  type: "save",
-  ...handlers,
-});
+/**
+ * These are the input configurations required from the merchant.
+ */
+const REQUIRED_INPUTS = {
+  onApprove: 'function',
+  createVaultSetupToken: 'function'
+}
+
+const validateSaveConfig = (config: SaveActionConfig): void => {
+  for (const [inputProp, inputType] of Object.entries(REQUIRED_INPUTS)) {
+    if (!config[inputProp] || typeof config[inputProp] !== inputType) {
+      throw new ValidationError(`Save action is missing the required '${inputProp}' callback`)  
+    }
+  }
+}
+
+/**
+ * Creating a Save action allows us to validate initial inputs from the merchant, and then return the resulting object. 
+ */
+export const createSaveAction: SaveAction = (config: SaveActionConfig) => {
+  validateSaveConfig(config)
+
+  return {
+    type: "save",
+    save: () => {
+      return ZalgoPromise.try(() => {
+        // basics for typing requirements. Implementation to come in next ticket.
+      })
+    }
+  }
+};
