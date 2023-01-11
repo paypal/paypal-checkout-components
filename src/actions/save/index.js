26f6d67daf6cdf96c946f35f4cfa6fc3a7ca1315
Shane Brunson <brunson.ws@gmail.com>
2023-01-11 09:34:58 -0400

    match action to proposed spec

--- src/actions/save/index.js
+++ src/actions/save/index.js
@@ -4,30 +4,10 @@ import { ZalgoPromise } from '@krakenjs/zalgo-promise/src';
 
 import { ValidationError } from "../../lib"
 
-type CardDetails = {|
-  number: ?string,
-  expiry: ?string,
-  name?: ?string,
-  security_code?: ?string,
-  type?: string,
-  postalCode?: ?string,
-  billingAddress? : string,
-|};
-
 type SaveActionConfig = {|
   createVaultSetupToken: () => ZalgoPromise<string>,
   onApprove: ({| vaultSetupToken: string |}) => void,
 |};
-
-export type onErrorCallback = (error: string) => void
-
-/* 
- * The onError function passed here is the `onError` callback provided to the component, e.g. Hosted Card Fields.
-*/
-export type SaveAction = (SaveActionConfig) => ({|
-  type: "save",
-  save: (onError: onErrorCallback, paymentSourceDetails: CardDetails) => ZalgoPromise<void>,
-|});
 
 /**
  * These are the input configurations required from the merchant.
@@ -48,27 +28,11 @@ const validateSaveConfig = (config: SaveActionConfig): void => {
 /**
  * Creating a Save action allows us to validate initial inputs from the merchant, and then return the resulting object. 
  */
-export const createSaveAction: SaveAction = (config: SaveActionConfig) => {
+export const createSaveAction = (config: SaveActionConfig): {|type: 'SAVE', ... SaveActionConfig|} => {
   validateSaveConfig(config)
 
   return {
-    type: "save",
-    save: (onError, paymentSourceDetails) => {
-      const { createVaultSetupToken } = config;
-    
-      if (!onError || !paymentSourceDetails) {
-        return ZalgoPromise.reject(new ValidationError("Missing args to #save"))
-      }
-
-      return ZalgoPromise.try(() => {
-        createVaultSetupToken()
-        .then((/* emptySetupToken */) => {
-          // take token and call our PP endpoint with it
-        }).catch((/* error */) => {
-          // TODO: Let's make sure we stringify this error safely/idiomatically - Do we define errors elsewhere?
-          return onError("Unable to retrieve setup token from 'createVaultSetupToken'")
-        })
-      })  
-    }
+    type: "SAVE",
+    ...config,
   }
 };
