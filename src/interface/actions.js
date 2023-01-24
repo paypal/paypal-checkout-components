e0ebe6e9019da7845166b47b862387459ab90b73
Shane Brunson <brunson.ws@gmail.com>
2023-01-24 09:38:02 -0400

    tweaks for save action (#2074)

--- src/interface/actions.js
+++ src/interface/actions.js
@@ -1,15 +1,8 @@
 /* @flow */
 
-import { isPayPalDomain } from "@paypal/sdk-client/src";
-
 import type { LazyProtectedExport } from "../types";
 import { createSaveAction, type CreateSaveAction } from "../actions/save";
-
-function protectedExport<T>(xport: T): ?T {
-  if (isPayPalDomain()) {
-    return xport;
-  }
-}
+import {protectedExport} from '../lib'
 
 export const actions: LazyProtectedExport<{|
   Save: CreateSaveAction,
