7c10a2cb0662e834eb568dc4bbaaffb56fdeb846
Dustin Jones <dustintaylorjones@gmail.com>
2023-02-03 16:01:17 -0500

    chore: create experiment wrapper

--- src/lib/getLogoCDNExperiment.js
+++ src/lib/getLogoCDNExperiment.js
@@ -2,7 +2,29 @@
 
 import { createExperiment } from "@paypal/sdk-client/src";
 import type { Experiment } from "@krakenjs/belter/src";
+import type { ChildType } from "@krakenjs/jsx-pragmatic/src";
+import { LOGO_COLOR } from "@paypal/sdk-logos/src";
 
 export function getLogoCDNExperiment(): Experiment {
-  return createExperiment("enable_logo_cdn", 0);
+  return createExperiment("enable_logo_cdn", 100);
+}
+
+type LogoProps = {|
+  logoColor?: $Values<typeof LOGO_COLOR>,
+  optional: void | boolean,
+  loadFromCDN?: boolean,
+|};
+
+export function enableLogoCDNExperiment(
+  logo: (LogoProps) => ChildType,
+  logoOptions: LogoProps
+): ChildType {
+  if (__WEB__) {
+    const logoCDNExperiment = getLogoCDNExperiment();
+    const loadFromCDN = logoCDNExperiment.isEnabled();
+
+    return logo({ ...logoOptions, loadFromCDN });
+  }
+
+  return logo(logoOptions);
 }
