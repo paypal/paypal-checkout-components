7564c438a873dc70c4a45fec972e24adb6a97d95
Dustin Jones <dustintaylorjones@gmail.com>
2023-05-16 16:15:23 -0400

    chore: add logging for deprecated integrations

--- src/lib/errors.js
+++ src/lib/errors.js
@@ -20,6 +20,8 @@ export function checkForDeprecatedIntegration() {
 
     for (const script of scripts) {
         if (script.attributes.type?.value === 'application/x-component') {
+            warn('deprecated_integration_application_xcomponent');
+
             // eslint-disable-next-line no-console
             console.error(`
                 This integration pattern using '<script type="application/x-component">' is no longer supported.
