699338fda28ff93ee2963d12532987557fc2a914
Shraddha Shah <spshah@paypal.com>
2023-11-07 16:48:17 -0800

    update loader config

--- src/connect/component.jsx
+++ src/connect/component.jsx
@@ -1,31 +1,35 @@
 /* @flow */
-import { loadConnectScript } from "@paypal/connect-loader-component";
+import { loadAxo } from "@paypal/connect-loader-component";
 import {
   getClientID,
   getClientMetadataID,
   getUserIDToken,
+  getSessionID,
 } from "@paypal/sdk-client/src";
-
-// eslint-disable-next-line flowtype/no-weak-types
-export type ConnectComponent = any;
 
 // TODO: What's the expected structure/approach for this interface. It's not a zoid
 // scenario, so what do we return?
 // -> Looks like it returns a function that accepts the props
 // How do we define the input of merchant params here?
 export const getConnectComponent = async (merchantProps) => {
-  const cmid = getClientMetadataID();
+  const cmid = getClientMetadataID() ?? getSessionID();
   const clientID = getClientID();
   const userIdToken = getUserIDToken();
   // TODO: Sort out integration specifics for inputs
   try {
-    const loadResult = await loadConnectScript();
-    return window.braintree.connect.create({
+    const loadResult = await loadAxo({
+      client: { getVersion: () => "3.97.3-connect-alpha.6.1" },
+      minified: false,
+    });
+
+    return await window.braintree.connect.create({
       ...loadResult.metadata,
       ...merchantProps,
-      cmid,
+      authorization: userIdToken,
+      deviceData: JSON.stringify({
+        correlation_id: cmid,
+      }),
       clientID,
-      userIdToken,
     });
   } catch (error) {
     return new Error(error);
