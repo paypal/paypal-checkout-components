d75daed1831f8f259a2b8e0be76b3d25a9470fc5
Shraddha Shah <spshah@paypal.com>
2023-11-08 13:56:26 -0800

    proposed payload for PPCP AXO loader

--- src/connect/component.jsx
+++ src/connect/component.jsx
@@ -18,20 +18,25 @@ export const getConnectComponent = async (merchantProps) => {
   // TODO: Sort out integration specifics for inputs
   try {
     const loadResult = await loadAxo({
-      client: { getVersion: () => "3.97.3-connect-alpha.6.1" },
+      client: { getVersion: () => "3.97.3-connect-alpha.6.1" }, // this is currently supported for BT
+      sdkVersion: "", // PPCP proposal: string instead of function
       minified: false,
     });
-
+    // FPTI: sdkversion, fraudnet info
     return await window.braintree.connect.create({
-      ...loadResult.metadata,
-      ...merchantProps,
-      authorization: userIdToken,
-      deviceData: JSON.stringify({
-        correlation_id: cmid,
-      }),
-      clientID,
+      ...loadResult.metadata, // returns a localeURL for assets
+      ...merchantProps, // AXO specific props
+      ppcp: {
+        userIdToken, // <merchant-specified-via-data-user-id-token>
+        clientID, // <merchant-specified-to-SDK-on-query-param>
+        clientMetadataID: cmid, // <merchant-specified-via-data-client-metadata-id>
+        fraudnet: () => {
+          return "";
+        }, // Pattern TBD
+      },
     });
   } catch (error) {
+    // FPTI Log here
     return new Error(error);
   }
 };
