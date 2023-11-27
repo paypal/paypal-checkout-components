3b4711bfa370a70430f055bc549e516773ce701f
Charlie Dibble <cgdibble@gmail.com>
2023-11-27 11:51:02 -0800

    add some basic analytics

--- src/connect/component.jsx
+++ src/connect/component.jsx
@@ -4,43 +4,89 @@ import {
   getClientID,
   getClientMetadataID,
   getUserIDToken,
+  getLogger,
 } from "@paypal/sdk-client/src";
 
-// TODO: What's the expected structure/approach for this interface. It's not a zoid
-// scenario, so what do we return?
-// -> Looks like it returns a function that accepts the props
-// How do we define the input of merchant params here?
+const sendCountMetric = ({
+  dimensions,
+  event = "unused",
+  name,
+  value = 1,
+}: {|
+  event?: string,
+  name: string,
+  value?: number,
+  dimensions: {
+    [string]: mixed,
+  },
+  // $FlowIssue return type
+|}) =>
+  getLogger().metric({
+    dimensions,
+    metricEventName: event,
+    metricNamespace: name,
+    metricValue: value,
+    metricType: "counter",
+  });
+
 // $FlowFixMe
 export const getConnectComponent = async (merchantProps) => {
+  sendCountMetric({
+    name: "pp.app.paypal_sdk.connect.init.count",
+  });
+
   const cmid = getClientMetadataID();
   const clientID = getClientID();
   const userIdToken = getUserIDToken();
   const { metadata } = merchantProps;
-  // TODO: Sort out integration specifics for inputs
+
+  let loadResult = {};
   try {
-    const loadResult = await loadAxo({
+    loadResult = await loadAxo({
       platform: "PPCP",
       btSdkVersion: "3.97.3-connect-alpha.6.1",
       minified: false,
       metadata,
     });
+  } catch (error) {
+    sendCountMetric({
+      name: "pp.app.paypal_sdk.connect.init.error.count",
+      event: "error",
+      dimensions: {
+        errorName: "connect_load_error",
+      },
+    });
+  }
 
+  try {
     // FPTI: sdkversion, fraudnet info
-    return await window.braintree.connect.create({
+    const connect = await window.braintree.connect.create({
       ...loadResult.metadata, // returns a localeURL for assets
       ...merchantProps, // AXO specific props
       platformOptions: {
         platform: "PPCP",
-        userIdToken, // <merchant-specified-via-data-user-id-token>
-        clientID, // <merchant-specified-to-SDK-on-query-param>
-        clientMetadataID: cmid, // <merchant-specified-via-data-client-metadata-id>
+        userIdToken,
+        clientID,
+        clientMetadataID: cmid,
         fraudnet: () => {
           return "";
-        }, // Pattern TBD
+        },
       },
     });
+
+    sendCountMetric({
+      name: "pp.app.paypal_sdk.connect.init.success.count",
+      event: "success",
+    });
+    return connect;
   } catch (error) {
-    // FPTI Log here
+    sendCountMetric({
+      name: "pp.app.paypal_sdk.connect.init.error.count",
+      event: "error",
+      dimensions: {
+        errorName: "connect_init_error",
+      },
+    });
     return new Error(error);
   }
 };
