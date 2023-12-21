05b51a2152ec19d41f54951b04017ddbfd83cbd8
Jesse Shawl <jshawl@paypal.com>
2023-12-21 11:39:23 -0600

    use hosted-buttons

--- src/interface/hosted-buttons.js
+++ src/interface/hosted-buttons.js
@@ -4,8 +4,20 @@ import {
   getHostedButtonsComponent,
   type HostedButtonsComponent,
 } from "../hosted-buttons";
-import type { LazyExport } from "../types";
+import { getButtonsComponent } from "../zoid/buttons";
+import { getCheckoutComponent, type CheckoutComponent } from "../zoid/checkout";
+import type { LazyExport, LazyProtectedExport } from "../types";
+import { protectedExport } from "../lib";
 
 export const HostedButtons: LazyExport<HostedButtonsComponent> = {
   __get__: () => getHostedButtonsComponent(),
 };
+
+export const Checkout: LazyProtectedExport<CheckoutComponent> = {
+  __get__: () => protectedExport(getCheckoutComponent()),
+};
+
+export function setup() {
+  getButtonsComponent();
+  getCheckoutComponent();
+}
