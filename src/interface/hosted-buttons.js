e2810edbab8e4b55095a35ceec2d6eb29742c1b0
Jesse Shawl <jshawl@paypal.com>
2024-01-05 09:10:13 -0600

    fix inline guest w/ HostedButtons

--- src/interface/hosted-buttons.js
+++ src/interface/hosted-buttons.js
@@ -3,6 +3,10 @@
 import { getHostedButtonsComponent } from "../hosted-buttons";
 import type { HostedButtonsComponent } from "../hosted-buttons/types";
 import { getButtonsComponent } from "../zoid/buttons";
+import {
+  getCardFormComponent,
+  type CardFormComponent,
+} from "../zoid/card-form";
 import { getCheckoutComponent, type CheckoutComponent } from "../zoid/checkout";
 import type { LazyExport, LazyProtectedExport } from "../types";
 import { protectedExport } from "../lib";
@@ -13,6 +17,10 @@ export const HostedButtons: LazyExport<HostedButtonsComponent> = {
 
 export const Checkout: LazyProtectedExport<CheckoutComponent> = {
   __get__: () => protectedExport(getCheckoutComponent()),
+};
+
+export const CardForm: LazyProtectedExport<CardFormComponent> = {
+  __get__: () => protectedExport(getCardFormComponent()),
 };
 
 export function setup() {
