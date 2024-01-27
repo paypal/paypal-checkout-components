033e3aeb8084cf40dc1111804a886ce9ea24e213
Ravi Shekhar <ravshekhar@paypal.com>
2024-01-27 12:15:04 -0600

    Add recommended payment to fpti payload

--- src/api/shopper-insights/component.js
+++ src/api/shopper-insights/component.js
@@ -150,11 +150,18 @@ export function getShopperInsightsComponent(): ShopperInsightsComponent {
             false;
           const isVenmoRecommended =
             (venmo?.eligible_in_paypal_network && venmo?.recommended) || false;
+          const fptiRecommendedPaymentPayload = {
+            paypal: isPayPalRecommended ? "1" : "0",
+            venmo: isVenmoRecommended ? "1" : "0",
+          };
 
           getLogger().track({
             [FPTI_KEY.TRANSITION]: FPTI_TRANSITION.SHOPPER_INSIGHTS_API_SUCCESS,
             [FPTI_KEY.EVENT_NAME]: FPTI_TRANSITION.SHOPPER_INSIGHTS_API_SUCCESS,
             [FPTI_KEY.RESPONSE_DURATION]: (Date.now() - startTime).toString(),
+            [FPTI_KEY.RECOMMENDED_PAYMENT]: JSON.stringify(
+              fptiRecommendedPaymentPayload
+            ),
           });
 
           sendCountMetric({
