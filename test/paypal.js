9e0bcb39bd97c160b36b7c5ee557e94e057d0f57
Jesse Shawl <jshawl@paypal.com>
2024-02-20 15:09:40 -0600

    fix typecheck

--- test/paypal.js
+++ test/paypal.js
@@ -10,11 +10,11 @@ import * as paypalPaymentFields from "../src/interface/payment-fields"; // eslin
 
 // the enable-funding=venmo flag is temporarily needed for the venmo experiment
 // the enable-funding=applepay flag is needed for applepay vertical stack and standalone buttons to render
-document.currentScript.setAttribute(
+document.currentScript?.setAttribute(
   "src",
   "test/paypal.js?client-id=test&enable-funding=venmo,applepay"
 );
-document.currentScript.setAttribute("data-csp-nonce", "test");
+document.currentScript?.setAttribute("data-csp-nonce", "test");
 
 window.mockDomain = "mock://www.paypal.com";
 
