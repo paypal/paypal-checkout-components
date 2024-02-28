404a37d055528e23fe927427b06435f66728bc77
Nate Bierdeman <nbierdeman@paypal.com>
2024-02-28 16:41:52 -0500

    the event listener was not the culprit

--- src/setup.js
+++ src/setup.js
@@ -8,7 +8,7 @@ import { FPTI } from './constants';
 import { initLogger, checkForCommonErrors, setLogLevel, stringifyError,
     stringifyErrorMessage, getResourceLoadTime, isPayPalDomain, isEligible,
     getDomainSetting, once, openMetaFrame, precacheRememberedFunding,
-    getCurrentScript, getRememberedFunding } from './lib';
+    getCurrentScript, getRememberedFunding, checkForDeprecatedIntegration } from './lib';
 import { Button } from './button';
 import { Checkout } from './checkout';
 import { pptm } from './external';
@@ -162,7 +162,7 @@ export const init = once(({ precacheRemembered }) => {
 
     initLogger();
 
-    // window.addEventListener('load', checkForDeprecatedIntegration);
+    window.addEventListener('load', checkForDeprecatedIntegration);
 
     if (pptm.shouldCreateInitialPptmScript()) {
         pptm.createPptmScript();
