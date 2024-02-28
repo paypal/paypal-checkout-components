83f8f2921a2ec052d4f3bb49e72c4f6242a1300e
Jesse Shawl <jshawl@paypal.com>
2024-02-28 08:56:30 -0600

    render horizontal inline guest

--- src/ui/buttons/buttons.test.jsx
+++ src/ui/buttons/buttons.test.jsx
@@ -16,40 +16,74 @@ const props = {
     paypal: {
       eligible: true,
     },
+    venmo: {
+      eligible: true,
+    },
   },
 };
 
+const selectors = {
+  inlineGuest: `[${ATTRIBUTE.FUNDING_SOURCE}='card']`,
+  paypal: `[${ATTRIBUTE.FUNDING_SOURCE}='paypal'`,
+  poweredBy: `.${CLASS.POWERED_BY}`,
+  tagline: `.${CLASS.TAGLINE}`,
+  venmo: `[${ATTRIBUTE.FUNDING_SOURCE}='venmo'`,
+};
+
+const render = (buttonProps) => {
+  const container = document.createElement("div");
+  // $FlowIssue not all required props are needed for this test
+  container.appendChild(Buttons(buttonProps).render(dom()));
+  return container;
+};
+
 describe("Buttons", () => {
-  describe("style.layout = 'vertical", () => {
+  describe("style.layout = 'vertical'", () => {
     it("renders inline guest and paypal powered by", () => {
-      const container = document.createElement("div");
-      // $FlowIssue
-      container.appendChild(Buttons(props).render(dom()));
-      expect(
-        container.querySelector(`[${ATTRIBUTE.FUNDING_SOURCE}='card']`)
-      ).toBeTruthy();
-      expect(container.querySelector(`.${CLASS.POWERED_BY}`)).toBeTruthy();
-      expect(container.querySelector(`.${CLASS.TAGLINE}`)).toBeFalsy();
+      const container = render(props);
+      expect(container.querySelector(selectors.inlineGuest)).toBeTruthy();
+      expect(container.querySelector(selectors.poweredBy)).toBeTruthy();
+      expect(container.querySelector(selectors.tagline)).toBeFalsy();
     });
   });
 
   describe("style.layout = 'horizontal'", () => {
+    const style = {
+      layout: "horizontal",
+    };
+    it("renders the first two funding sources with a tagline", () => {
+      const container = render({
+        ...props,
+        style,
+      });
+      expect(container.querySelector(selectors.paypal)).toBeTruthy();
+      expect(container.querySelector(selectors.venmo)).toBeTruthy();
+      expect(container.querySelector(selectors.inlineGuest)).toBeFalsy();
+      expect(container.querySelector(selectors.poweredBy)).toBeFalsy();
+      expect(container.querySelector(selectors.tagline)).toBeTruthy();
+    });
+    it("renders inline guest with powered by if eligible", () => {
+      // $FlowIssue fundingEligibility props are optional
+      const { card, paypal } = props.fundingEligibility;
+      const container = render({
+        fundingEligibility: {
+          card,
+          paypal,
+        },
+        style,
+      });
+      expect(container.querySelector(selectors.inlineGuest)).toBeTruthy();
+      expect(container.querySelector(selectors.poweredBy)).toBeTruthy();
+      expect(container.querySelector(selectors.tagline)).toBeFalsy();
+    });
     it("does not render inline guest or paypal powered by if layout is horizontal", () => {
-      const container = document.createElement("div");
-      container.appendChild(
-        // $FlowIssue
-        Buttons({
-          ...props,
-          style: {
-            layout: "horizontal",
-          },
-        }).render(dom())
-      );
-      expect(
-        container.querySelector(`[${ATTRIBUTE.FUNDING_SOURCE}='card']`)
-      ).toBeFalsy();
-      expect(container.querySelector(`.${CLASS.POWERED_BY}`)).toBeFalsy();
-      expect(container.querySelector(`.${CLASS.TAGLINE}`)).toBeTruthy();
+      const container = render({
+        ...props,
+        style,
+      });
+      expect(container.querySelector(selectors.inlineGuest)).toBeFalsy();
+      expect(container.querySelector(selectors.poweredBy)).toBeFalsy();
+      expect(container.querySelector(selectors.tagline)).toBeTruthy();
     });
   });
 });
