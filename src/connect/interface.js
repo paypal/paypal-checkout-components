d4be8020081c2af420fa03f6bc27cb63b9e12640
Shane Brunson <wbrunson@paypal.com>
2024-02-28 10:24:26 -0600

    fix fastlane interface (#2347)

--- src/connect/interface.js
+++ src/connect/interface.js
@@ -1,14 +1,10 @@
 /* @flow */
-/* eslint-disable eslint-comments/disable-enable-pair */
-/* eslint-disable no-restricted-globals, promise/no-native */
 
-import { memoize, type Memoized } from "@krakenjs/belter/src";
+import { memoize } from "@krakenjs/belter/src";
 
 import { getConnectComponent } from "./component";
 
 type MerchantProps = {||};
-// This needs to be typed, this is coming from the fastlane team i believe
-type FastlaneExternalComponent = {||};
 
 type ConnectComponent = (merchantProps: MerchantProps) => ConnectComponent;
 // $FlowFixMe
@@ -18,7 +14,8 @@ export const Connect: (merchantProps: MerchantProps) => ConnectComponent =
     return await getConnectComponent(merchantProps);
   });
 
-export const Fastlane = (
-  merchantProps: MerchantProps
-): Memoized<() => Promise<FastlaneExternalComponent>> =>
-  memoize(() => getConnectComponent(merchantProps));
+export const Fastlane: (merchantProps: MerchantProps) => ConnectComponent =
+  memoize(async (merchantProps: MerchantProps): ConnectComponent => {
+    // $FlowFixMe
+    return await getConnectComponent(merchantProps);
+  });
