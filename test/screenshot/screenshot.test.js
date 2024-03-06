c3490a93d21d76849f53bcde20f5fa59ccedcb0d
Nate Bierdeman <nbierdeman@paypal.com>
2024-03-06 12:11:51 -0500

    revert screenshot tests to original state

--- test/screenshot/screenshot.test.js
+++ test/screenshot/screenshot.test.js
@@ -25,23 +25,17 @@ const USER_AGENTS = {
 jest.setTimeout(120000);
 
 const setupBrowserPage = (async () => {
-    console.log('>>> setupBrowserPage has been invoked');
     const { browser, page } = await openPage(await webpackCompile(BASE_SCREENSHOT_TEST));
-    console.log('>>> post setupBrowserPage openPage log');
 
-    // for (const filename of await fs.readdir(IMAGE_DIR)) {
-    //     if (filename.endsWith('-old.png')) {
-    //         await fs.unlink(`${ IMAGE_DIR }/${ filename }`);
-    //     }
-    // }
-    console.log('>>> post setupBrowserPage for...of log');
+    for (const filename of await fs.readdir(IMAGE_DIR)) {
+        if (filename.endsWith('-old.png')) {
+            await fs.unlink(`${ IMAGE_DIR }/${ filename }`);
+        }
+    }
 
-    // await page.evaluate(() => {
-    //     window.paypal.setup({ env: 'test' });
-    // });
-
-    console.log(`>>> setupBrowserPage browser: ${ browser }`);
-    console.log(`>>> setupBrowserPage page: ${ page }`);
+    await page.evaluate(() => {
+        window.paypal.setup({ env: 'test' });
+    });
 
     return { browser, page };
 })();
@@ -49,7 +43,6 @@ const setupBrowserPage = (async () => {
 beforeAll(() => setupBrowserPage);
 
 afterAll(async () => {
-    console.log('>>> afterAll');
     const { browser } = await setupBrowserPage;
     await browser.close();
 });
@@ -58,101 +51,101 @@ const total = buttonConfigs.length;
 let index = 1;
 const config = buttonConfigs[0];
 
-// for (const config of buttonConfigs) {
-const filename = config.filename || dotifyToString(config) || 'base';
+for (const config of buttonConfigs) {
+    const filename = config.filename || dotifyToString(config) || 'base';
 
-test(`Render button with ${ filename }`, async () => {
-    const { page } = await setupBrowserPage;
+    test(`Render button with ${ filename }`, async () => {
+        const { page } = await setupBrowserPage;
 
-    // fasten up the animation
-    await page._client.send('Animation.setPlaybackRate', { playbackRate: 12 });
-    const filepath = `${ IMAGE_DIR }/${ filename }.png`;
-    const diffpath  = `${ IMAGE_DIR }/${ filename }-old.png`;
+        // fasten up the animation
+        await page._client.send('Animation.setPlaybackRate', { playbackRate: 12 });
+        const filepath = `${ IMAGE_DIR }/${ filename }.png`;
+        const diffpath  = `${ IMAGE_DIR }/${ filename }-old.png`;
 
-    const { x, y, width, height } = await page.evaluate(async (options, userAgents) => {
+        const { x, y, width, height } = await page.evaluate(async (options, userAgents) => {
 
-        // $FlowFixMe
-        document.body.innerHTML = '';
+            // $FlowFixMe
+            document.body.innerHTML = '';
 
-        const container = window.document.createElement('div');
-        window.document.body.appendChild(container);
+            const container = window.document.createElement('div');
+            window.document.body.appendChild(container);
 
-        if (options.container) {
-            container.style.width = `${ options.container.width }px`;
-        }
-
-        if (options.userAgent) {
-            window.navigator.mockUserAgent = userAgents[options.userAgent];
-        }
-
-        let decorate;
-
-        if (options.button.funding && options.button.funding.allowed && options.button.funding.allowed.indexOf(window.paypal.FUNDING.VENMO) !== -1) {
-            decorate = window.paypal.Button.props.funding.decorate;
-            window.paypal.Button.props.funding.decorate = (funding = {}) => {
-                // eslint-disable-next-line prefer-object-spread
-                return Object.assign({}, funding, { remembered: [ window.paypal.FUNDING.VENMO ] });
-            };
-        }
-
-        // eslint-disable-next-line prefer-object-spread
-        window.paypal.Button.render(Object.assign({
-            payment() {
-                // pass
-            },
-            onAuthorize() {
-                // pass
+            if (options.container) {
+                container.style.width = `${ options.container.width }px`;
             }
-        }, options.button), container);
 
-        await new Promise(resolve => setTimeout(resolve, 100));
-        
-        const rect = container.querySelector('iframe').getBoundingClientRect();
-
-        delete window.navigator.mockUserAgent;
-
-        if (decorate) {
-            // eslint-disable-next-line require-atomic-updates
-            window.paypal.Button.props.funding.decorate = decorate;
-        }
-
-        return {
-            x:      rect.left,
-            y:      rect.top,
-            width:  rect.width,
-            height: rect.height
-        };
-
-    }, config, USER_AGENTS);
-
-    const existingExists = await fs.exists(filepath);
-
-    const [ screenshot, existing ] = await Promise.all([
-        takeScreenshot(page, { x, y, width, height }),
-        existingExists ? readPNG(filepath) : null
-    ]);
-
-    if (existing) {
-        const delta = await diffPNG(screenshot, existing);
-
-        if (delta > DIFF_THRESHOLD) {
-            await existing.write(diffpath);
-            await screenshot.write(filepath);
-
-            let imgurUrl = '';
-
-            // eslint-disable-next-line no-process-env
-            if (process.env.TRAVIS) {
-                imgurUrl = await uploadToImgur(filepath);
+            if (options.userAgent) {
+                window.navigator.mockUserAgent = userAgents[options.userAgent];
             }
+
+            let decorate;
+
+            if (options.button.funding && options.button.funding.allowed && options.button.funding.allowed.indexOf(window.paypal.FUNDING.VENMO) !== -1) {
+                decorate = window.paypal.Button.props.funding.decorate;
+                window.paypal.Button.props.funding.decorate = (funding = {}) => {
+                    // eslint-disable-next-line prefer-object-spread
+                    return Object.assign({}, funding, { remembered: [ window.paypal.FUNDING.VENMO ] });
+                };
+            }
+
+            // eslint-disable-next-line prefer-object-spread
+            window.paypal.Button.render(Object.assign({
+                payment() {
+                    // pass
+                },
+                onAuthorize() {
+                    // pass
+                }
+            }, options.button), container);
+
+            await new Promise(resolve => setTimeout(resolve, 100));
             
-            throw new Error(`Button style changed with delta of ${ delta } for configuration:\n\n${ JSON.stringify(config, null, 4) }\n\nSee ${ diffpath } or ${ imgurUrl || '' }`);
-        }
+            const rect = container.querySelector('iframe').getBoundingClientRect();
 
-    } else {
-        await screenshot.write(filepath);
-    }
-    index += 1;
-    console.log(`Generating button screenshot: ${ index } / ${ total }`);
-});
-// }
+            delete window.navigator.mockUserAgent;
+
+            if (decorate) {
+                // eslint-disable-next-line require-atomic-updates
+                window.paypal.Button.props.funding.decorate = decorate;
+            }
+
+            return {
+                x:      rect.left,
+                y:      rect.top,
+                width:  rect.width,
+                height: rect.height
+            };
+
+        }, config, USER_AGENTS);
+
+        const existingExists = await fs.exists(filepath);
+
+        const [ screenshot, existing ] = await Promise.all([
+            takeScreenshot(page, { x, y, width, height }),
+            existingExists ? readPNG(filepath) : null
+        ]);
+
+        if (existing) {
+            const delta = await diffPNG(screenshot, existing);
+
+            if (delta > DIFF_THRESHOLD) {
+                await existing.write(diffpath);
+                await screenshot.write(filepath);
+
+                let imgurUrl = '';
+
+                // eslint-disable-next-line no-process-env
+                if (process.env.TRAVIS) {
+                    imgurUrl = await uploadToImgur(filepath);
+                }
+                
+                throw new Error(`Button style changed with delta of ${ delta } for configuration:\n\n${ JSON.stringify(config, null, 4) }\n\nSee ${ diffpath } or ${ imgurUrl || '' }`);
+            }
+
+        } else {
+            await screenshot.write(filepath);
+        }
+        index += 1;
+        console.log(`Generating button screenshot: ${ index } / ${ total }`);
+    });
+}
