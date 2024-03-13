34e811acd6b7c303761060f8df6c5054df48b362
Nate Bierdeman <nbierdeman@paypal.com>
2024-03-13 10:00:58 -0400

    Rename sdkIntegrationSource buttons zoid prop (#2334)
    * sdkIntegrationSource -> jsSdkLibrary
    
    * Alphabetize
    
    * Set jsSdkLibrary value to getJsSdkLibrary
    
    * Bump sdk-client version

--- src/zoid/buttons/component.jsx
+++ src/zoid/buttons/component.jsx
@@ -39,7 +39,7 @@ import {
   getDisableSetCookie,
   getExperimentation,
   getSDKAttribute,
-  getSDKIntegrationSource,
+  getJsSdkLibrary,
 } from "@paypal/sdk-client/src";
 import {
   rememberFunding,
@@ -610,6 +610,13 @@ export const getButtonsComponent: () => ButtonsComponent = memoize(() => {
         value: getIntent,
       },
 
+      jsSdkLibrary: {
+        type: "string",
+        queryParam: true,
+        required: false,
+        value: getJsSdkLibrary,
+      },
+
       locale: {
         type: "object",
         queryParam: true,
@@ -774,13 +781,6 @@ export const getButtonsComponent: () => ButtonsComponent = memoize(() => {
         required: false,
         value: getCorrelationID,
         queryParam: true,
-      },
-
-      sdkIntegrationSource: {
-        type: "string",
-        queryParam: true,
-        required: false,
-        value: getSDKIntegrationSource,
       },
 
       sdkMeta: {
