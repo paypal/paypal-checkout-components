20d3ba8998d6957964787783a8fced890f38929d
Shraddha Shah <spshah@paypal.com>
2024-09-10 13:30:56 -0700

    add protected export

--- src/three-domain-secure/interface.js
+++ src/three-domain-secure/interface.js
@@ -1,7 +1,8 @@
 /* @flow */
 import type { LazyExport } from "../types";
 import { getThreeDomainSecure } from "./component";
+import { protectedExport } from "../lib";
 
 export const ThreeDomainSecureComp: LazyExport<> = {
-  __get__: () => getThreeDomainSecure(),
+  __get__: () => protectedExport(getThreeDomainSecure()),
 };
