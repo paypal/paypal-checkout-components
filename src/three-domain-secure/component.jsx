0c8233d97026800c52811bd2cd8b50c46cfbc277
Shraddha Shah <spshah@paypal.com>
2024-09-16 15:22:31 -0700

    add sdk-client-token auth for 3DS comp

--- src/three-domain-secure/component.jsx
+++ src/three-domain-secure/component.jsx
@@ -1,20 +1,31 @@
 /* @flow */
-import { getLogger } from "@paypal/sdk-client/src";
+import { getLogger, getSDKToken } from "@paypal/sdk-client/src";
 import { FPTI_KEY } from "@paypal/sdk-constants/src";
 
+import { ValidationError } from "../lib";
+
 export const getThreeDomainSecure = (): Function => {
+  console.log("getThreeDomainSecure");
+  const sdkToken = getSDKToken();
   const ThreeDomainSecureAuth = () => {
-    // eslint-disable-next-line no-console
-    console.log("Three Domain Secure Called");
-    // Make a Zoid component and introduce methods here
-    // onSuccess
-    // onCancel
-    // onClose
-    getLogger()
-      .info("three domain secure v2 invoked")
-      .track({
-        [FPTI_KEY.TRANSITION]: "three_DS_auth_v2",
-      });
+    if (sdkToken) {
+      // eslint-disable-next-line no-console
+      console.log("Three Domain Secure Called");
+      // Make a Zoid component and introduce methods here
+      // onSuccess
+      // onCancel
+      // onClose
+      getLogger()
+        .info("three domain secure v2 invoked")
+        .track({
+          [FPTI_KEY.TRANSITION]: "three_DS_auth_v2",
+        });
+    } else {
+      throw new ValidationError(
+        `script data attribute sdk-client-token is required but was not passed`
+      );
+    }
   };
+
   return ThreeDomainSecureAuth;
 };
--- src/three-domain-secure/component.test.js
+++ src/three-domain-secure/component.test.js
@@ -1,18 +1,38 @@
 /* @flow */
 import { describe, expect, it, vi } from "vitest";
-
+import { getLogger, getSDKToken } from "@paypal/sdk-client/src";
+import { ValidationError } from "../lib";
 import { getThreeDomainSecure } from "./component";
 
+vi.mock("@paypal/sdk-client/src", () => ({
+  getSDKToken: vi.fn(),
+  getLogger: vi.fn(() => ({
+    info: vi.fn().mockReturnThis(),
+    track: vi.fn().mockReturnThis(),
+    flush: vi.fn().mockReturnThis(),
+  })),
+}));
+vi.mock("../lib", () => ({
+  ValidationError: vi.fn(),
+}));
 describe("getThreeDomainSecure returns ThreeDomainSecureComponent", () => {
+  it("should throw an error if sdkToken is not present", () => {
+    getSDKToken.mockReturnValue(undefined);
+    const ThreeDomainSecureComponent = getThreeDomainSecure();
+    expect(() => ThreeDomainSecureComponent()).toThrowError(ValidationError);
+    expect(ValidationError).toHaveBeenCalledWith(
+      `script data attribute sdk-client-token is required but was not passed`
+    );
+  });
   it("should return the ThreeDomainSecure component and log the correct message", async () => {
     // eslint-disable-next-line no-empty-function
     const consoleSpy = vi.spyOn(console, "log").mockImplementation(() => {});
-
-    const ThreeDomainSecureComponent = await getThreeDomainSecure();
+    getSDKToken.mockReturnValue("84ghb8984");
+    const ThreeDomainSecureComponent = getThreeDomainSecure();
     expect(typeof ThreeDomainSecureComponent).toBe("function");
 
     // Call the returned component and check the console log
-    ThreeDomainSecureComponent();
+    await ThreeDomainSecureComponent();
     expect(consoleSpy).toHaveBeenCalledWith("Three Domain Secure Called");
 
     consoleSpy.mockRestore();
