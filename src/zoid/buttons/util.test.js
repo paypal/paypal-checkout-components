711e41c99325986afeddbced76a0d3246381bea0
Spencer Sablan <spencertsablan@gmail.com>
2024-09-26 10:49:00 -0700

    getPerformanceProp function / type / tests

---
+++ src/zoid/buttons/util.test.js
@@ -0,0 +1,30 @@
+/* @flow */
+
+import { describe } from "vitest";
+import { getPerformanceProp } from "./util";
+
+describe("getPerformanceProp", () => {
+  const buttonSessionID = "button-session-id";
+
+  beforeEach(() => {
+    performance.clearMarks();
+  });
+
+  test("should create object containing performance data", () => {
+    const startTime = 1;
+    const performanceEntryName = `${buttonSessionID}_buttons-first-render`;
+
+    performance.mark(performanceEntryName, { startTime });
+
+    const perfomanceProp = getPerformanceProp(buttonSessionID);
+
+    expect(perfomanceProp).toEqual({
+      firstRenderStartTime: startTime,
+    });
+  });
+
+  test("firstRenderStartTime should be undefined if the performance entry name is not found", () => {
+    const performanceProp = getPerformanceProp(buttonSessionID);
+    expect(performanceProp).toEqual({});
+  });
+});
