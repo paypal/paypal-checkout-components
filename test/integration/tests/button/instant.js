6ae7916d3198ac33a7f3d3d795444fe68b3ad8c7
Hai-Yen Nguyen <haiynguyen@paypal.com>
2024-10-01 13:03:51 -0500

    debugging why it failing in instant.js

--- test/integration/tests/button/instant.js
+++ test/integration/tests/button/instant.js
@@ -22,11 +22,43 @@ for (const flow of ["popup", "iframe"]) {
       destroyTestContainer();
     });
 
-    it("should render a button into a container and click on the button instantly, then complete the checkout", () => {
-      return wrapPromise(({ expect, error }) => {
-        const buttonRender = window.paypal
+    it.only("should render a button into a container and click on the button instantly, then complete the checkout", (done) => {
+      wrapPromise(({ expect, error }) => {
+        window.paypal
           .Buttons({
-            test: { flow },
+            test: {
+              // flow,
+              onRender() {
+                try {
+                  const frame = document.querySelector(
+                    "#testContainer iframe.prerender-frame"
+                  );
+
+                  if (!frame) {
+                    done(new Error(`Can not find prerender frame`));
+                  }
+
+                  // $FlowFixMe
+                  const win = frame.contentWindow;
+                  // if (!win) {
+                  //   done(new Error(`win is not winning`));
+                  // }
+
+                  setTimeout(() => {
+                    const button = win.document.querySelector(`[role="link"]`);
+
+                    frame.click();
+                    if (!button) {
+                      done(new Error(`not buttonn!!`));
+                    }
+                    button.click();
+                    done();
+                  }, 0);
+                } catch (e) {
+                  done(e);
+                }
+              },
+            },
             onApprove: expect("onApprove"),
             onCancel: error("onCancel"),
             onError: error("onError", (err) => {
@@ -34,31 +66,81 @@ for (const flow of ["popup", "iframe"]) {
             }),
           })
           .render("#testContainer");
+      });
+    });
 
-        const frame = document.querySelector(
-          "#testContainer iframe.prerender-frame"
-        );
+    it("should render a button into a container and press space button on the button instantly, then complete the checkout", (done) => {
+      wrapPromise(({ expect, error }) => {
+        const buttonRender = window.paypal
+          .Buttons({
+            test: {
+              flow,
+              onRender() {
+                try {
+                  const frame = document.querySelector(
+                    "#testContainer iframe.prerender-frame"
+                  );
 
-        if (!frame) {
-          throw new Error(`Can not find prerender frame`);
-        }
+                  if (!frame) {
+                    throw new Error(`Can not find prerender frame`);
+                  }
 
-        // $FlowFixMe
-        const win = frame.contentWindow;
-        const button = win.document.querySelector('[role="link"]');
+                  // $FlowFixMe
+                  const win = frame.contentWindow;
+                  const button = win.document.querySelector('[role="link"]');
 
-        frame.click();
-        button.click();
+                  frame.click();
+                  button.dispatchEvent(
+                    new KeyboardEvent("keypress", { keyCode: 32 })
+                  );
+                  done();
+                } catch (e) {
+                  done(e);
+                }
+              },
+            },
+            onApprove: expect("onApprove"),
+            onCancel: error("onCancel"),
+            onError: error("onError", (err) => {
+              throw err;
+            }),
+          })
+          .render("#testContainer");
 
         return buttonRender;
       });
     });
 
-    it("should render a button into a container and press space button on the button instantly, then complete the checkout", () => {
-      return wrapPromise(({ expect, error }) => {
+    it("should render a button into a container and press enter button on the button instantly, then complete the checkout", (done) => {
+      wrapPromise(({ expect, error }) => {
         const buttonRender = window.paypal
           .Buttons({
-            test: { flow },
+            test: {
+              flow,
+              onRender() {
+                try {
+                  const frame = document.querySelector(
+                    "#testContainer iframe.prerender-frame"
+                  );
+
+                  if (!frame) {
+                    throw new Error(`Can not find prerender frame`);
+                  }
+
+                  // $FlowFixMe
+                  const win = frame.contentWindow;
+                  const button = win.document.querySelector('[role="link"]');
+
+                  frame.click();
+                  button.dispatchEvent(
+                    new KeyboardEvent("keypress", { keyCode: 13 })
+                  );
+                  done();
+                } catch (e) {
+                  done(e);
+                }
+              },
+            },
             onApprove: expect("onApprove"),
             onCancel: error("onCancel"),
             onError: error("onError", (err) => {
@@ -66,53 +148,6 @@ for (const flow of ["popup", "iframe"]) {
             }),
           })
           .render("#testContainer");
-
-        const frame = document.querySelector(
-          "#testContainer iframe.prerender-frame"
-        );
-
-        if (!frame) {
-          throw new Error(`Can not find prerender frame`);
-        }
-
-        // $FlowFixMe
-        const win = frame.contentWindow;
-        const button = win.document.querySelector('[role="link"]');
-
-        frame.click();
-        button.dispatchEvent(new KeyboardEvent("keypress", { keyCode: 32 }));
-
-        return buttonRender;
-      });
-    });
-
-    it("should render a button into a container and press enter button on the button instantly, then complete the checkout", () => {
-      return wrapPromise(({ expect, error }) => {
-        const buttonRender = window.paypal
-          .Buttons({
-            test: { flow },
-            onApprove: expect("onApprove"),
-            onCancel: error("onCancel"),
-            onError: error("onError", (err) => {
-              throw err;
-            }),
-          })
-          .render("#testContainer");
-
-        const frame = document.querySelector(
-          "#testContainer iframe.prerender-frame"
-        );
-
-        if (!frame) {
-          throw new Error(`Can not find prerender frame`);
-        }
-
-        // $FlowFixMe
-        const win = frame.contentWindow;
-        const button = win.document.querySelector('[role="link"]');
-
-        frame.click();
-        button.dispatchEvent(new KeyboardEvent("keypress", { keyCode: 13 }));
 
         return buttonRender;
       });
