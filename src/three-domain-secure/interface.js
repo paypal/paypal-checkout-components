673d98dc88c3714320b2c2f82b331c1794186ce2
Mervin Choun <mchoun@paypal.com>
2024-10-15 14:08:38 -0400

    refactor threedomainsecure component to class

--- src/three-domain-secure/interface.js
+++ src/three-domain-secure/interface.js
@@ -1,13 +1,24 @@
 /* @flow */
+import { getLogger, getSDKToken } from "@paypal/sdk-client/src";
 import { type ZoidComponent } from "@krakenjs/zoid/src";
 
 import type { LazyExport } from "../types";
 import { protectedExport } from "../lib";
 
-import { getThreeDomainSecure } from "./component";
+import {
+  ThreeDomainSecureComponent,
+  type ThreeDomainSecureComponentInterface,
+} from "./component";
 
-type ThreeDomainSecureAuth = ZoidComponent<void>;
-
-export const ThreeDomainSecureComponent: LazyExport<ThreeDomainSecureAuth> = {
-  __get__: () => protectedExport(getThreeDomainSecure()),
-};
+export const ThreeDomainSecureClient: LazyExport<ThreeDomainSecureComponentInterface> =
+  {
+    __get__: () =>
+      protectedExport(
+        new ThreeDomainSecureComponent({
+          logger: getLogger(),
+          sdkConfig: {
+            sdkToken: getSDKToken(),
+          },
+        })
+      ),
+  };
