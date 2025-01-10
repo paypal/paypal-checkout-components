9e7717aaff39149bc5368107705c1b3c96ba2731
Ravi Shekhar <ravshekhar@paypal.com>
2025-01-10 12:26:58 -0600

    update resume flow contract

--- src/lib/appSwitchResume.js
+++ src/lib/appSwitchResume.js
@@ -1,5 +1,6 @@
 /* @flow */
 import { FUNDING } from "@paypal/sdk-constants/src";
+import { parseQuery } from "@krakenjs/belter/src";
 
 import { APP_SWITCH_RETURN_HASH } from "../constants";
 
@@ -16,46 +17,47 @@ export type AppSwitchResumeParams = {|
 |};
 
 export function getAppSwitchResumeParams(): AppSwitchResumeParams | null {
-  const urlHash = String(window.location.hash).replace("#", "");
+  const hashString = window.location.hash && window.location.hash.slice(1);
+  const [hash, queryString] = hashString.split("?");
+
   const isPostApprovalAction = [
     APP_SWITCH_RETURN_HASH.ONAPPROVE,
     APP_SWITCH_RETURN_HASH.ONCANCEL,
     APP_SWITCH_RETURN_HASH.ONERROR,
-  ].includes(urlHash);
+  ].includes(hash);
   if (!isPostApprovalAction) {
     return null;
   }
-  // eslint-disable-next-line compat/compat
-  const search = new URLSearchParams(window.location.search);
-  const orderID = search.get("orderID");
-  const payerID = search.get("payerID");
-  const buttonSessionID = search.get("buttonSessionID");
-  const billingToken = search.get("billingToken");
-  const paymentID = search.get("paymentID");
-  const subscriptionID = search.get("subscriptionID");
-  const vaultSetupToken = search.get("vaultSetupToken");
-  const fundingSource = search.get("fundingSource");
-  if (buttonSessionID) {
-    const params: AppSwitchResumeParams = {
-      orderID,
-      buttonSessionID,
-      payerID,
-      billingToken,
-      paymentID,
-      subscriptionID,
-      // URLSearchParams get returns as string,
-      // but below code excepts a value from list of string.
-      // $FlowIgnore[incompatible-type]
-      fundingSource,
-      vaultSetupToken,
-      // the isPostApprovalAction already ensures
-      // that the function will exit if url hash is not one of supported values.
-      // $FlowIgnore[incompatible-type]
-      checkoutState: urlHash,
-    };
-    return params;
-  }
-  return null;
+
+  const {
+    token,
+    PayerID,
+    buttonSessionID,
+    billingToken,
+    paymentID,
+    subscriptionID,
+    vaultSetupToken,
+    fundingSource,
+  } = parseQuery(queryString);
+
+  const params: AppSwitchResumeParams = {
+    orderID: token,
+    buttonSessionID,
+    payerID: PayerID,
+    billingToken,
+    paymentID,
+    subscriptionID,
+    // URLSearchParams get returns as string,
+    // but below code excepts a value from list of string.
+    // $FlowIgnore[incompatible-type]
+    fundingSource,
+    vaultSetupToken,
+    // the isPostApprovalAction already ensures
+    // that the function will exit if url hash is not one of supported values.
+    // $FlowIgnore[incompatible-type]
+    checkoutState: hash,
+  };
+  return params;
 }
 
 export function isAppSwitchResumeFlow(): boolean {
