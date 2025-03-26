44b7f0b97ddcacf2d10a19ba9d2fa781fdb1933c
Hai-Yen Nguyen <hynn510@gmail.com>
2025-03-26 09:59:34 -0500

    update font-family + update font-weight for poweredBy (#2482)
    * update font-family + update font-weight for poweredBy
    
    * adding system-ui, -apple-system, Segoe UI to font-family + update CLASS.TAGLINE font-weight to 400
    
    * update font-family for page.js
    
    * update font-family for template.jsx

--- src/ui/buttons/styles/page.js
+++ src/ui/buttons/styles/page.js
@@ -4,7 +4,7 @@ import { CLASS } from "../../../constants";
 
 export const pageStyle = `
     html, body {
-        font-family: PayPalOpen-Regular, Helvetica, Arial, "Liberation Sans", sans-serif;
+        font-family: PayPalPlain-Regular, system-ui, -apple-system, Roboto, "Segoe UI", Helvetica-Neue, Helvetica, Arial, sans-serif;
         padding: 0;
         margin: 0;
         width: 100%;
