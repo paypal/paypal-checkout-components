2d9325636d2c3356b6a6c1c57a38fd91f30b96e5
Brian Tedder <btedder@paypal.com>
2025-04-01 14:17:10 -0500

    add app switch state

--- src/zoid/buttons/component.jsx
+++ src/zoid/buttons/component.jsx
@@ -44,7 +44,6 @@ import {
   wasShopperInsightsUsed,
   isPayPalTrustedUrl,
   getSDKInitTime,
-  getSDKToken,
 } from "@paypal/sdk-client/src";
 import {
   rememberFunding,
@@ -110,11 +109,9 @@ export type ButtonsComponent = ZoidComponent<
   ButtonExtensions
 >;
 
-// $FlowIssue
 export const getButtonsComponent: () => ButtonsComponent = memoize(() => {
   const queriedEligibleFunding = [];
 
-  // $FlowIssue
   return create({
     tag: "paypal-buttons",
     url: () => `${getPayPalDomain()}${__PAYPAL_CHECKOUT__.__URI__.__BUTTONS__}`,
@@ -123,7 +120,10 @@ export const getButtonsComponent: () => ButtonsComponent = memoize(() => {
     getExtensions: (parent) => {
       return {
         hasReturned: () => {
-          return isAppSwitchResumeFlow();
+          return (
+            isAppSwitchResumeFlow() &&
+            !parent.getHelpers()?.state?.appSwitchState
+          );
         },
         resume: () => {
           const resumeFlowParams = getAppSwitchResumeParams();
@@ -319,22 +319,28 @@ export const getButtonsComponent: () => ButtonsComponent = memoize(() => {
         type: "function",
         sendToChild: false,
         queryParam: false,
-        value: () => (event) => {
-          sendPostRobotMessageToButtonIframe({
-            eventName: "paypal-hashchange",
-            payload: {
-              url: event.newURL,
-            },
-          });
-        },
+        value:
+          ({ state }) =>
+          (event) => {
+            console.log("hash change", state);
+            state.appSwitchState = "returned";
+            sendPostRobotMessageToButtonIframe({
+              eventName: "paypal-hashchange",
+              payload: {
+                url: event.newURL,
+              },
+            });
+          },
       },
 
       listenForHashChanges: {
         type: "function",
         queryParam: false,
         value:
-          ({ props }) =>
+          ({ props, state }) =>
           () => {
+            state.appSwitchState = "pending";
+
             window.addEventListener("hashchange", props.hashChangeHandler);
           },
       },
@@ -353,27 +359,33 @@ export const getButtonsComponent: () => ButtonsComponent = memoize(() => {
         type: "function",
         sendToChild: false,
         queryParam: false,
-        value: () => () => {
-          sendPostRobotMessageToButtonIframe({
-            eventName: "paypal-visibilitychange",
-            payload: {
-              url: window.location.href,
-              // eslint-disable-next-line compat/compat
-              visibilityState: document.visibilityState,
-            },
-          });
-        },
+        value:
+          ({ state }) =>
+          () => {
+            state.appSwitchState = "returned";
+
+            sendPostRobotMessageToButtonIframe({
+              eventName: "paypal-visibilitychange",
+              payload: {
+                url: window.location.href,
+                // eslint-disable-next-line compat/compat
+                visibilityState: document.visibilityState,
+              },
+            });
+          },
       },
 
       listenForVisibilityChange: {
         type: "function",
         queryParam: false,
         value:
-          ({ props }) =>
+          ({ props, state }) =>
           () => {
+            state.appSwitchState = state.appSwitchState ?? "pending";
+
             window.addEventListener(
               "visibilitychange",
-              props.visibilityChangeHandler
+              props.hashChangeHandler
             );
           },
       },
@@ -386,7 +398,7 @@ export const getButtonsComponent: () => ButtonsComponent = memoize(() => {
           () => {
             window.removeEventListener(
               "visibilitychange",
-              props.visibilityChangeHandler
+              props.hashChangeHandler
             );
           },
       },
@@ -1117,12 +1129,6 @@ export const getButtonsComponent: () => ButtonsComponent = memoize(() => {
         queryParam: true,
         sendToChild: false,
         value: getSDKMeta,
-      },
-
-      sdkToken: {
-        type: "string",
-        required: false,
-        value: getSDKToken,
       },
 
       /**
