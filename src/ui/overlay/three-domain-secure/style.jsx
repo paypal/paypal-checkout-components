646e893f478ba09de55bf4f86142333bdbdaffc8
Delaney Barrow <dbarrow@paypal.com>
2025-05-12 11:48:05 -0500

    Updated fonts with system values

--- src/ui/overlay/three-domain-secure/style.jsx
+++ src/ui/overlay/three-domain-secure/style.jsx
@@ -113,7 +113,7 @@ export function getContainerStyle({ uid }: {| uid: string |}): string {
         }
 
         #${uid} .paypal-checkout-modal {
-            font-family: PayPal Plain, SF Pro Text, Roboto, Segoe UI, Helvetica-Neue, Helvetica, Arial, sans-serif;
+            font-family: PayPal Plain, system-ui, -apple-system, Roboto, Segoe UI, Helvetica-Neue, Helvetica, Arial, sans-serif;
             font-size: 14px;
             text-align: center;
 
