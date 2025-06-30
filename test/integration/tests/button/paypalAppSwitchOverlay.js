41110b98276e32dcc923111ff85733af4431b634
Steve Mask <mnicpt@gmail.com>
2025-06-30 19:48:45 -0400

    fix(test): showPayPalAppSwitchOverlay

--- test/integration/tests/button/paypalAppSwitchOverlay.js
+++ test/integration/tests/button/paypalAppSwitchOverlay.js
@@ -72,16 +72,13 @@ describe(`PayPal app switch overlay`, () => {
 
             getElementRecursive(".paypal-checkout-continue").click();
 
-            // timeout is to allow time for focus to be called
-            setTimeout(() => {
-              if (!focusCalled) {
-                done(new Error("Expected focus function to be called"));
-              }
+            if (!focusCalled) {
+              done(new Error("Expected focus function to be called"));
+            }
 
-              xprops.hidePayPalAppSwitchOverlay({ close: noop });
+            xprops.hidePayPalAppSwitchOverlay({ close: noop });
 
-              done();
-            }, 300);
+            done();
           },
         },
         onApprove(): void {
