39e78ef8deebcf3c96958c1fa021ef03c980d845
Delaney Barrow <dbarrow@paypal.com>
2025-08-26 15:07:34 -0500

    removed undefined as valid funding source

--- src/ui/buttons/props.js
+++ src/ui/buttons/props.js
@@ -753,7 +753,7 @@ export function hasInvalidScriptOptionsForFullRedesign({
 }: {|
   fundingSource?: ?$Values<typeof FUNDING>,
 |}): boolean {
-  const validFundingSourcesForRedesign = [FUNDING.PAYPAL, undefined];
+  const validFundingSourcesForRedesign = [FUNDING.PAYPAL];
 
   if (validFundingSourcesForRedesign.includes(fundingSource)) {
     return false;
