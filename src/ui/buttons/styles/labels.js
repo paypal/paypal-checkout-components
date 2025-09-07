4896ce9d4659c12db64057989f3ce065754d7e08
nikrom17 <nikrom17@gmail.com>
2025-09-07 09:58:10 -0500

    update venmo font styles

--- src/ui/buttons/styles/labels.js
+++ src/ui/buttons/styles/labels.js
@@ -65,7 +65,7 @@ export const labelStyle = `
         font-family: PayPal Pro Book, system-ui, -apple-system, Roboto, "Segoe UI", Helvetica-Neue, Helvetica, Arial, sans-serif;
     }
 
-    .${CLASS.BUTTON_REBRAND} div[data-funding-source=venmo] .${
+    .${CLASS.BUTTON_REBRAND}, div[data-funding-source=venmo] .${
   CLASS.BUTTON_LABEL
 } {
         font-family: Scto Grotesk A, system-ui, -apple-system, Roboto, "Segoe UI", Helvetica-Neue, Helvetica, Arial, sans-serif;
