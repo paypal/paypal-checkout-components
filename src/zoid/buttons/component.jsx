90a5dd13c5a83979c9fa88be6b1c93cbb62723cd
vishala <thallapallyvishala@gmail.com>
2026-01-27 12:57:47 -0600

    feat(buttons): add button disability changes

--- src/zoid/buttons/component.jsx
+++ src/zoid/buttons/component.jsx
@@ -1370,6 +1370,12 @@ export const getButtonsComponent: () => ButtonsComponent = memoize(() => {
         queryParam: true,
         value: getUserAgent,
       },
+
+      disabled: {
+        type: "boolean",
+        queryParam: true,
+        required: false,
+      },
     },
 
     exports: {
