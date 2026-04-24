b073653fb3c5151af3107a14495ed2d45a69bd1f
vishala <thallapallyvishala@gmail.com>
2026-04-24 13:54:52 -0500

    changes

--- src/funding/pp/config.jsx
+++ src/funding/pp/config.jsx
@@ -1,29 +1,14 @@
 /* @flow */
 /** @jsx node */
 
-import { node, type ChildType } from "@krakenjs/jsx-pragmatic/src";
-import {
-  PPRebrandLogoExternalImage,
-  PPRebrandLogoInlineSVG,
-} from "@paypal/sdk-logos/src";
+import { PPRebrandLogoExternalImage } from "@paypal/sdk-logos/src";
 import { LOGO_COLOR } from "@paypal/sdk-logos/src/constants";
 
 import { type FundingSourceConfig } from "../common";
 
-// Mark component for PP (PayPal monogram)
-function Mark(): ChildType {
-  return __WEB__ ? (
-    // $FlowFixMe - JSX pragma compatibility
-    <PPRebrandLogoExternalImage logoColor={LOGO_COLOR.BLUE} />
-  ) : (
-    // $FlowFixMe - JSX pragma compatibility
-    <PPRebrandLogoInlineSVG />
-  );
-}
-
 export function getPPConfig(): FundingSourceConfig {
   // $FlowFixMe - pp is a marks-only config, only needs Mark component
   return {
-    Mark,
+    Mark: () => <PPRebrandLogoExternalImage logoColor={LOGO_COLOR.BLUE} />,
   };
 }
