6a439cb8b6d7ebb13beb366868ff74cc6fe061fc
jayasd <jayasd@paypal.com>
2026-06-29 16:09:39 +0530

    update UTs to match latest styles

--- src/ui/buttons/styles/styleUtils.test.constants.js
+++ src/ui/buttons/styles/styleUtils.test.constants.js
@@ -404,13 +404,14 @@ const expectedResizeLabelDisableMaxHeightStylesXXXL = {
 // ============================================================================
 
 // expected rebrand disable max height responsive styles variables
+// labelHeight = Math.round(height * REBRAND_LABEL_HEIGHT_RATIO) where ratio = 0.5
 const expectedRebrandDisableMaxHeightStylesTiny = {
   APMHeight: 18,
   applePayHeight: 25,
   buttonHeight: 25,
   disableHeightStyle:
     BUTTON_DISABLE_MAX_HEIGHT_STYLE[BUTTON_DISABLE_MAX_HEIGHT_SIZE.TINY],
-  labelHeight: 19,
+  labelHeight: 13,
   fontSize: 10,
   marginTop: 1,
   pillBorderRadius: 13,
@@ -424,7 +425,7 @@ const expectedRebrandDisableMaxHeightStylesSmall = {
   buttonHeight: 30,
   disableHeightStyle:
     BUTTON_DISABLE_MAX_HEIGHT_STYLE[BUTTON_DISABLE_MAX_HEIGHT_SIZE.SMALL],
-  labelHeight: 23,
+  labelHeight: 15,
   fontSize: 11,
   marginTop: 1,
   pillBorderRadius: 15,
@@ -440,7 +441,7 @@ const expectedRebrandDisableMaxHeightStylesMediumSmall = {
     BUTTON_DISABLE_MAX_HEIGHT_STYLE[
       BUTTON_DISABLE_MAX_HEIGHT_SIZE.MEDIUM_SMALL
     ],
-  labelHeight: 27,
+  labelHeight: 18,
   fontSize: 13,
   marginTop: 1,
   pillBorderRadius: 18,
@@ -454,7 +455,7 @@ const expectedRebrandDisableMaxHeightStylesMediumBig = {
   buttonHeight: 40,
   disableHeightStyle:
     BUTTON_DISABLE_MAX_HEIGHT_STYLE[BUTTON_DISABLE_MAX_HEIGHT_SIZE.MEDIUM_BIG],
-  labelHeight: 30,
+  labelHeight: 20,
   fontSize: 14,
   marginTop: 1,
   pillBorderRadius: 20,
@@ -468,7 +469,7 @@ const expectedRebrandDisableMaxHeightStylesLargeSmall = {
   buttonHeight: 45,
   disableHeightStyle:
     BUTTON_DISABLE_MAX_HEIGHT_STYLE[BUTTON_DISABLE_MAX_HEIGHT_SIZE.LARGE_SMALL],
-  labelHeight: 34,
+  labelHeight: 23,
   fontSize: 16,
   marginTop: 2,
   pillBorderRadius: 23,
@@ -482,7 +483,7 @@ const expectedRebrandDisableMaxHeightStylesLargeBig = {
   buttonHeight: 50,
   disableHeightStyle:
     BUTTON_DISABLE_MAX_HEIGHT_STYLE[BUTTON_DISABLE_MAX_HEIGHT_SIZE.LARGE_BIG],
-  labelHeight: 38,
+  labelHeight: 25,
   fontSize: 18,
   marginTop: 2,
   pillBorderRadius: 25,
@@ -496,7 +497,7 @@ const expectedRebrandDisableMaxHeightStylesXL = {
   buttonHeight: 55,
   disableHeightStyle:
     BUTTON_DISABLE_MAX_HEIGHT_STYLE[BUTTON_DISABLE_MAX_HEIGHT_SIZE.XL],
-  labelHeight: 42,
+  labelHeight: 28,
   fontSize: 20,
   marginTop: 2,
   pillBorderRadius: 28,
@@ -510,7 +511,7 @@ const expectedRebrandDisableMaxHeightStylesXXL = {
   buttonHeight: 65,
   disableHeightStyle:
     BUTTON_DISABLE_MAX_HEIGHT_STYLE[BUTTON_DISABLE_MAX_HEIGHT_SIZE.XXL],
-  labelHeight: 49,
+  labelHeight: 33,
   fontSize: 23,
   marginTop: 2,
   pillBorderRadius: 33,
@@ -524,7 +525,7 @@ const expectedRebrandDisableMaxHeightStylesXXXL = {
   buttonHeight: 75,
   disableHeightStyle:
     BUTTON_DISABLE_MAX_HEIGHT_STYLE[BUTTON_DISABLE_MAX_HEIGHT_SIZE.XXXL],
-  labelHeight: 57,
+  labelHeight: 38,
   fontSize: 27,
   marginTop: 3,
   pillBorderRadius: 38,
@@ -540,7 +541,6 @@ const expectedRebrandDisableMaxHeightStylesXXXL = {
 const expectedRebrandedResponsiveStylesExtraSmall = {
   style: BUTTON_REDESIGN_STYLE[BUTTON_REDESIGN_SIZE.EXTRA_SMALL],
   buttonHeight: 20,
-  pillBorderRadius: 10,
   gap: 3,
   defaultHeight: 20,
   minHeight: 20,
@@ -548,13 +548,12 @@ const expectedRebrandedResponsiveStylesExtraSmall = {
   minWidth: 50,
   minDualWidth: 300,
   maxWidth: 75,
-  fontSize: 12,
+  fontSize: 10,
 };
 
 const expectedRebrandedResponsiveStylesTiny = {
   style: BUTTON_REDESIGN_STYLE[BUTTON_REDESIGN_SIZE.TINY],
   buttonHeight: 25,
-  pillBorderRadius: 13,
   gap: 3,
   defaultHeight: 25,
   minHeight: 25,
@@ -562,27 +561,25 @@ const expectedRebrandedResponsiveStylesTiny = {
   minWidth: 75,
   minDualWidth: 300,
   maxWidth: 200,
-  fontSize: 12,
+  fontSize: 10,
 };
 
 const expectedRebrandedResponsiveStylesSmall = {
   style: BUTTON_REDESIGN_STYLE[BUTTON_REDESIGN_SIZE.SMALL],
   buttonHeight: 35,
-  pillBorderRadius: 18,
-  gap: 3,
+  gap: 4,
   defaultHeight: 35,
   minHeight: 30,
   maxHeight: 35,
   minWidth: 200,
   minDualWidth: 300,
   maxWidth: 250,
-  fontSize: 14,
+  fontSize: 12,
 };
 
 const expectedRebrandedResponsiveStylesMediumSmall = {
   style: BUTTON_REDESIGN_STYLE[BUTTON_REDESIGN_SIZE.MEDIUM_SMALL],
   buttonHeight: 35,
-  pillBorderRadius: 18,
   gap: 4,
   defaultHeight: 35,
   minHeight: 35,
@@ -590,27 +587,25 @@ const expectedRebrandedResponsiveStylesMediumSmall = {
   minWidth: 250,
   minDualWidth: 300,
   maxWidth: 300,
-  fontSize: 16,
+  fontSize: 14,
 };
 
 const expectedRebrandedResponsiveStylesMediumBig = {
   style: BUTTON_REDESIGN_STYLE[BUTTON_REDESIGN_SIZE.MEDIUM_BIG],
   buttonHeight: 45,
-  pillBorderRadius: 23,
-  gap: 4,
+  gap: 5,
   defaultHeight: 45,
   minHeight: 40,
   maxHeight: 45,
   minWidth: 300,
   minDualWidth: 300,
   maxWidth: 350,
-  fontSize: 16,
+  fontSize: 14,
 };
 
 const expectedRebrandedResponsiveStylesLargeSmall = {
   style: BUTTON_REDESIGN_STYLE[BUTTON_REDESIGN_SIZE.LARGE_SMALL],
   buttonHeight: 45,
-  pillBorderRadius: 23,
   gap: 5,
   defaultHeight: 45,
   minHeight: 45,
@@ -618,35 +613,33 @@ const expectedRebrandedResponsiveStylesLargeSmall = {
   minWidth: 350,
   minDualWidth: 300,
   maxWidth: 425,
-  fontSize: 18,
+  fontSize: 16,
 };
 
 const expectedRebrandedResponsiveStylesLargeBig = {
   style: BUTTON_REDESIGN_STYLE[BUTTON_REDESIGN_SIZE.LARGE_BIG],
-  buttonHeight: 45,
-  pillBorderRadius: 23,
+  buttonHeight: 50,
   gap: 5,
-  defaultHeight: 45,
+  defaultHeight: 50,
   minHeight: 50,
   maxHeight: 55,
   minWidth: 425,
   minDualWidth: 300,
   maxWidth: 500,
-  fontSize: 20,
+  fontSize: 18,
 };
 
 const expectedRebrandedResponsiveStylesXlSmall = {
   style: BUTTON_REDESIGN_STYLE[BUTTON_REDESIGN_SIZE.XL_SMALL],
   buttonHeight: 55,
-  pillBorderRadius: 28,
   gap: 6,
   defaultHeight: 55,
   minHeight: 55,
   maxHeight: 60,
   minWidth: 500,
   minDualWidth: 300,
-  maxWidth: 550,
-  fontSize: 22,
+  maxWidth: 750,
+  fontSize: 18,
 };
 
 const expectedRebrandedResponsiveStylesXlBig = {
