6af8eec05e48532e4de5a10932a6210eec64dd78
nikrom17 <nikrom17@gmail.com>
2026-07-21 12:30:21 -0500

    restore not-passed-to-child-iframe comment on visibilityChangeHandler

--- src/ui/buttons/props.js
+++ src/ui/buttons/props.js
@@ -598,7 +598,9 @@ export type ButtonProps = {|
   hashChangeHandler: (event: any) => void,
   listenForVisibilityChange: () => void,
   removeListenerForVisibilityChanges: () => void,
+  // Not passed to child iframe
   visibilityChangeHandler: () => void,
+
   showPayPalAppSwitchOverlay: (args: ShowPayPalAppSwitchOverlay) => void,
   hidePayPalAppSwitchOverlay: (args: HidePayPalAppSwitchOverlay) => void,
 
